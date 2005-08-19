11bfd2559fab43a0feb5d16677dd76603c9526c2
Steve Ebersole <steve@hibernate.org>
2005-08-19 19:15:57 +0000

    file header

--- src/main/org/jboss/seam/annotations/BeginProcess.java
+++ src/main/org/jboss/seam/annotations/BeginProcess.java
@@ -1,3 +1,9 @@
+/*
+ * JBoss, Home of Professional Open Source
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
 package org.jboss.seam.annotations;
 
 import static java.lang.annotation.ElementType.METHOD;
