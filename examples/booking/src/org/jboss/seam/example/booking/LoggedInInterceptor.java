2be417a327162a518bdd1ebf2e0481ace53aa35e
Gavin King <gavin.king@gmail.com>
2005-08-21 19:35:32 +0000

    minor fix

--- examples/booking/src/org/jboss/seam/example/booking/LoggedInInterceptor.java
+++ examples/booking/src/org/jboss/seam/example/booking/LoggedInInterceptor.java
@@ -5,11 +5,16 @@ import javax.ejb.InvocationContext;
 
 import org.jboss.logging.Logger;
 import org.jboss.seam.annotations.Around;
+import org.jboss.seam.annotations.Within;
 import org.jboss.seam.contexts.Contexts;
 import org.jboss.seam.interceptors.AbstractInterceptor;
 import org.jboss.seam.interceptors.BijectionInterceptor;
+import org.jboss.seam.interceptors.ConversationInterceptor;
+import org.jboss.seam.interceptors.RemoveInterceptor;
+import org.jboss.seam.interceptors.ValidationInterceptor;
 
-@Around(BijectionInterceptor.class)
+@Around({BijectionInterceptor.class, ValidationInterceptor.class, ConversationInterceptor.class})
+@Within(RemoveInterceptor.class)
 public class LoggedInInterceptor extends AbstractInterceptor<LoggedIn>
 {
    private static final Logger log = Logger.getLogger(LoggedInInterceptor.class);
