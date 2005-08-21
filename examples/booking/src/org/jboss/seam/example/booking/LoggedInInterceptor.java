38276917845432069354d0abce7c458540aeeed8
Gavin King <gavin.king@gmail.com>
2005-08-21 21:15:08 +0000

    updated

--- examples/booking/src/org/jboss/seam/example/booking/LoggedInInterceptor.java
+++ examples/booking/src/org/jboss/seam/example/booking/LoggedInInterceptor.java
@@ -1,13 +1,13 @@
 //$Id$
 package org.jboss.seam.example.booking;
 
+import javax.ejb.AroundInvoke;
 import javax.ejb.InvocationContext;
 
 import org.jboss.logging.Logger;
 import org.jboss.seam.annotations.Around;
 import org.jboss.seam.annotations.Within;
 import org.jboss.seam.contexts.Contexts;
-import org.jboss.seam.interceptors.AbstractInterceptor;
 import org.jboss.seam.interceptors.BijectionInterceptor;
 import org.jboss.seam.interceptors.ConversationInterceptor;
 import org.jboss.seam.interceptors.RemoveInterceptor;
@@ -15,12 +15,12 @@ import org.jboss.seam.interceptors.ValidationInterceptor;
 
 @Around({BijectionInterceptor.class, ValidationInterceptor.class, ConversationInterceptor.class})
 @Within(RemoveInterceptor.class)
-public class LoggedInInterceptor extends AbstractInterceptor<LoggedIn>
+public class LoggedInInterceptor
 {
    private static final Logger log = Logger.getLogger(LoggedInInterceptor.class);
 
-   @Override
-   public Object aroundInvoke(InvocationContext invocation) throws Exception
+   @AroundInvoke
+   public Object checkLoggedIn(InvocationContext invocation) throws Exception
    {
       boolean isLoggedIn = Contexts.getSessionContext().get("loggedIn")!=null;
       if (isLoggedIn) 
