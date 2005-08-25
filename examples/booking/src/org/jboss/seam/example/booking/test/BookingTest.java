126ed0e95bcca381bf313e8e8c307912642149c0
Gavin King <gavin.king@gmail.com>
2005-08-25 04:31:45 +0000

    first full test

--- examples/booking/src/org/jboss/seam/example/booking/test/BookingTest.java
+++ examples/booking/src/org/jboss/seam/example/booking/test/BookingTest.java
@@ -3,6 +3,8 @@ package org.jboss.seam.example.booking.test;
 
 import java.util.Map;
 
+import javax.faces.model.DataModel;
+
 import org.jboss.seam.Component;
 import org.jboss.seam.components.ConversationManager;
 import org.jboss.seam.components.Settings;
@@ -20,10 +22,10 @@ public class BookingTest extends SeamTest
 {
    
    @Test
-   public void testStuff() throws Exception
+   public void testBookHotel() throws Exception
    {
       
-      new Script() {
+      String id = new Script() {
 
          HotelBooking hotelBooking;
          
@@ -51,11 +53,102 @@ public class BookingTest extends SeamTest
          @Override
          protected void renderResponse()
          {
+            DataModel dm = (DataModel) Contexts.getConversationContext().get("hotelsDataModel");
+            assert dm.isRowAvailable();
+            assert ( (Hotel) dm.getRowData() ).getCity().equals("NY");
+            assert dm.getRowCount()==1;
             assert "NY".equals( hotelBooking.getSearchString() );
             assert ConversationManager.instance().isLongRunningConversation();
          }
+         
+      }.run();
+      
+      new Script(id) {
 
+         @Override
+         protected void invokeApplication()
+         {
+            HotelBooking hotelBooking = (HotelBooking) Contexts.getConversationContext().get("hotelBooking");
+            String outcome = hotelBooking.selectHotel();
+            assert "selected".equals( outcome );
+         }
 
+         @Override
+         protected void renderResponse()
+         {
+            Hotel hotel = (Hotel) Contexts.getConversationContext().get("hotel");
+            assert hotel.getCity().equals("NY");
+            assert hotel.getZip().equals("11111");
+            assert ConversationManager.instance().isLongRunningConversation();
+         }
+         
+      }.run();
+      
+      new Script(id) {
+
+         @Override
+         protected void invokeApplication()
+         {
+            HotelBooking hotelBooking = (HotelBooking) Contexts.getConversationContext().get("hotelBooking");
+            String outcome = hotelBooking.bookHotel();
+            assert "book".equals( outcome );
+         }
+
+         @Override
+         protected void renderResponse()
+         {
+            Booking booking = (Booking) Contexts.getConversationContext().get("booking");
+            assert booking.getUser()!=null;
+            assert booking.getHotel()!=null;
+            assert booking.getHotel()==Contexts.getConversationContext().get("hotel");
+            assert booking.getUser()==Contexts.getSessionContext().get("user");
+            assert ConversationManager.instance().isLongRunningConversation();
+         }
+         
+      }.run();
+      
+      new Script(id) {
+         
+         @Override @SuppressWarnings("deprecation")
+         protected void updateModelValues() throws Exception
+         {
+            Booking booking = (Booking) Contexts.getConversationContext().get("booking");
+            booking.getCheckinDate().setDate(10); 
+            booking.getCheckoutDate().setDate(12);
+            booking.setCreditCard("1234567891021234");
+         }
+
+         @Override
+         protected void invokeApplication()
+         {
+            HotelBooking hotelBooking = (HotelBooking) Contexts.getConversationContext().get("hotelBooking");
+            String outcome = hotelBooking.setBookingDetails();
+            assert "success".equals( outcome );
+         }
+
+         @Override
+         protected void renderResponse()
+         {
+            assert ConversationManager.instance().isLongRunningConversation();
+         }
+         
+      }.run();
+      
+      new Script(id) {
+        
+         @Override
+         protected void invokeApplication()
+         {
+            HotelBooking hotelBooking = (HotelBooking) Contexts.getConversationContext().get("hotelBooking");
+            String outcome = hotelBooking.confirm();
+            assert "confirmed".equals( outcome );
+         }
+
+         @Override
+         protected void renderResponse()
+         {
+            assert !ConversationManager.instance().isLongRunningConversation();
+         }
          
       }.run();
       
