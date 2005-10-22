f803fb1fa8498323472f76de3d5fe6f070f11e7c
Christian Bauer <christian@hibernate.org>
2005-10-22 14:20:36 +0000

    NoEJB example running on Microcontainer

--- src/main/org/jboss/seam/microcontainer/HibernateFactory.java
+++ src/main/org/jboss/seam/microcontainer/HibernateFactory.java
@@ -2,39 +2,165 @@
 package org.jboss.seam.microcontainer;
 
 import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.SessionFactory;
+import org.hibernate.util.ReflectHelper;
+
+import java.util.*;
+import java.io.File;
 
 /**
- * A factory that bootstraps a Hiberate SessionFactory
- * 
+ * A factory that bootstraps a Hibernate SessionFactory.
+ * <p>
+ * Loads Hibernate configuration options by checking:
+ * <li>hibernate.properties in root of the classpath
+ * <li>hibernate.cfg.xml in root of the classpath
+ * <li>cfgResourceName as location of a cfg.xml file
+ * <li>factory-suplied cfgProperties options
+ * <p>
+ * Note that this factory only supports cfg.xml files <b>or</b>
+ * programmatic <tt>cfgProperties</tt> supplied to the factory.
+ * Any <tt>hibernate.properties</tt> are always loaded from the
+ * classpath.
+ * <p>
+ * Mapping metadata can be supplied through:
+ * <li>mappingClasses: equivalent to &lt;mapping class="..."/>
+ * <li>mappingFiles: equivalent to &lt;mapping file="..."/>
+ * <li>mappingJars: equivalent to &lt;mapping jar="..."/>
+ * <li>mappingPackages: equivalent to &lt;mapping package="..."/>
+ * <li>mappingResources: equivalent to &lt;mapping resource="..."/>
+ * <p>
+ * or through cfg.xml files.
+ * <p>
+ * The <tt>jndiProperties</tt> are convenience, the factory will
+ * automatically prefix regular JNDI properties for use as Hibernate
+ * configuration properties.
+ *
  * @author Gavin King
+ * @author Christian Bauer
  */
-public class HibernateFactory
-{
+public class HibernateFactory {
 
-   private String cfgResourceName;
-   
-   public Object getSessionFactory() throws Exception
-   {
-      AnnotationConfiguration acfg = new AnnotationConfiguration();
-      if (cfgResourceName==null) 
-      {
-         acfg.configure();
-      }
-      else
-      {
-         acfg.configure(cfgResourceName);
-      }
-      return acfg.buildSessionFactory();      
-   }
-   
-   public String getCfgResourceName()
-   {
-      return cfgResourceName;
-   }
+    private String cfgResourceName;
+    private Properties cfgProperties;
+    private Hashtable jndiProperties;
 
-   public void setCfgResourceName(String cfgFileName)
-   {
-      this.cfgResourceName = cfgFileName;
-   }
+    private List<String> mappingClasses;
+    private List<String> mappingFiles;
+    private List<String> mappingJars;
+    private List<String> mappingPackages;
+    private List<String> mappingResources;
+
+    public Object buildSessionFactory() throws Exception {
+        AnnotationConfiguration acfg = new AnnotationConfiguration();
+
+        // Programmatic configuration
+        if (cfgProperties != null) {
+            acfg.setProperties(cfgProperties);
+        }
+
+        // Prefix regular JNDI properties for Hibernate
+        if (jndiProperties != null) {
+            for (Iterator it = jndiProperties.entrySet().iterator(); it.hasNext();) {
+                Map.Entry entry = (Map.Entry)it.next();
+                acfg.setProperty(Environment.JNDI_PREFIX + "." + entry.getKey(),
+                                 (String)entry.getValue() );
+            }
+        }
+
+        // hibernate.cfg.xml configuration
+        if (cfgProperties == null && cfgResourceName == null) {
+            acfg.configure();
+        } else if (cfgProperties == null && cfgResourceName != null) {
+            acfg.configure(cfgResourceName);
+        }
+
+        // Mapping metadata
+        if (mappingClasses != null)
+            for(String className: mappingClasses)
+                acfg.addAnnotatedClass(ReflectHelper.classForName(className));
+
+        if (mappingFiles != null)
+            for(String fileName: mappingFiles)
+                acfg.addFile(fileName);
+
+        if (mappingJars != null)
+            for(String jarName: mappingJars)
+                acfg.addJar( new File(jarName) );
+
+        if (mappingPackages != null)
+            for(String packageName: mappingPackages)
+                acfg.addPackage(packageName);
+
+        if (mappingResources != null)
+            for(String resourceName: mappingResources)
+                acfg.addResource(resourceName);
+
+        return acfg.buildSessionFactory();
+    }
+
+    public String getCfgResourceName() {
+        return cfgResourceName;
+    }
+
+    public void setCfgResourceName(String cfgFileName) {
+        this.cfgResourceName = cfgFileName;
+    }
+
+    public Properties getCfgProperties() {
+        return cfgProperties;
+    }
+
+    public void setCfgProperties(Properties cfgProperties) {
+        this.cfgProperties = cfgProperties;
+    }
+
+    public List<String> getMappingClasses() {
+        return mappingClasses;
+    }
+
+    public void setMappingClasses(List<String> mappingClasses) {
+        this.mappingClasses = mappingClasses;
+    }
+
+    public List<String> getMappingFiles() {
+        return mappingFiles;
+    }
+
+    public void setMappingFiles(List<String> mappingFiles) {
+        this.mappingFiles = mappingFiles;
+    }
+
+    public List<String> getMappingJars() {
+        return mappingJars;
+    }
+
+    public void setMappingJars(List<String> mappingJars) {
+        this.mappingJars = mappingJars;
+    }
+
+    public List<String> getMappingPackages() {
+        return mappingPackages;
+    }
+
+    public void setMappingPackages(List<String> mappingPackages) {
+        this.mappingPackages = mappingPackages;
+    }
+
+    public List<String> getMappingResources() {
+        return mappingResources;
+    }
+
+    public void setMappingResources(List<String> mappingResources) {
+        this.mappingResources = mappingResources;
+    }
+
+    public Hashtable getJndiProperties() {
+        return jndiProperties;
+    }
+
+    public void setJndiProperties(Hashtable jndiProperties) {
+        this.jndiProperties = jndiProperties;
+    }
 
 }
