4ca7138a6ea3745d8fbae14e7421e78b65660c74
Gavin King <gavin.king@gmail.com>
2005-12-21 08:00:45 +0000

    improved test

--- examples/booking/src/org/jboss/seam/example/booking/test/BookingTest.java
+++ examples/booking/src/org/jboss/seam/example/booking/test/BookingTest.java
@@ -2,7 +2,6 @@
 package org.jboss.seam.example.booking.test;
 
 import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 
 import javax.faces.application.FacesMessage;
@@ -218,6 +217,28 @@ public class BookingTest extends SeamTest
             Booking booking = (Booking) bookings.getRowData();
             assert booking.getHotel().getCity().equals("NY");
             assert booking.getUser().getUsername().equals("gavin");
+            assert !Manager.instance().isLongRunningConversation();
+         }
+         
+      }.run();
+      
+      new Script() {
+         
+         @Override
+         protected void invokeApplication()
+         {
+            ListDataModel bookings = (ListDataModel) Component.getInstance("bookings", false);
+            bookings.setRowIndex(0);
+            BookingList bookingList = (BookingList) Component.getInstance("bookingList", true);
+            String outcome = bookingList.cancel();
+            assert "cancelled".equals( outcome );
+         }
+
+         @Override
+         protected void renderResponse()
+         {
+            ListDataModel bookings = (ListDataModel) Component.getInstance("bookings", true);
+            assert bookings.getRowCount()==0;
             assert !Manager.instance().isLongRunningConversation();
          }
          
--- examples/noejb/src/org/jboss/seam/example/noejb/test/BookingTest.java
+++ examples/noejb/src/org/jboss/seam/example/noejb/test/BookingTest.java
@@ -2,7 +2,6 @@
 package org.jboss.seam.example.noejb.test;
 
 import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 
 import javax.faces.application.FacesMessage;
@@ -220,6 +219,28 @@ public class BookingTest extends SeamTest
             Booking booking = (Booking) bookings.getRowData();
             assert booking.getHotel().getCity().equals("NY");
             assert booking.getUser().getUsername().equals("gavin");
+            assert !Manager.instance().isLongRunningConversation();
+         }
+         
+      }.run();
+      
+      new Script() {
+         
+         @Override
+         protected void invokeApplication()
+         {
+            ListDataModel bookings = (ListDataModel) Component.getInstance("bookings", false);
+            bookings.setRowIndex(0);
+            BookingListAction bookingList = (BookingListAction) Component.getInstance("bookingList", true);
+            String outcome = bookingList.cancel();
+            assert "cancelled".equals( outcome );
+         }
+
+         @Override
+         protected void renderResponse()
+         {
+            ListDataModel bookings = (ListDataModel) Component.getInstance("bookings", true);
+            assert bookings.getRowCount()==0;
             assert !Manager.instance().isLongRunningConversation();
          }
          
