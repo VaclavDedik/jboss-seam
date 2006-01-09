5bd85aafff73a19cb708235228a62c392e264b4d
Gavin King <gavin.king@gmail.com>
2006-01-09 03:57:21 +0000

    javadoc

--- src/main/org/jboss/seam/ScopeType.java
+++ src/main/org/jboss/seam/ScopeType.java
@@ -17,15 +17,54 @@ import org.jboss.seam.contexts.Contexts;
  */
 public enum ScopeType 
 {
+   /**
+    * The stateless psuedo-context.
+    */
    STATELESS,
+   /**
+    * The event (request) context. Spans a server request,
+    * from restore view to render response.
+    */
    EVENT,
+   /**
+    * The page context. Begins during the invoke application
+    * phase prior to rendering a page, and lasts until the end 
+    * of any invoke application phase of a faces request 
+    * originating from that page. Non-faces requests do not
+    * propagate the page scope.
+    */
    PAGE,
+   /**
+    * The conversation context. Spans multiple requests from
+    * the same browser window, demarcated by @Begin and @End
+    * methods. A conversation context is propagated by
+    * any faces request, or by any request that specifies
+    * a conversation id as a request parameter. The conversation
+    * context is not available during the restore view phase.
+    */
    CONVERSATION,
+   /**
+    * The session context. (A servlet login session.)
+    */
    SESSION,
+   /**
+    * The application context (Servlet context.)
+    */
    APPLICATION,
+   /**
+    * The business process context. Spans multiple conversations
+    * with multiple users, demarcated by the start and end states
+    * of the business process definition.
+    */
    BUSINESS_PROCESS,
+   /**
+    * Indicates that the scope is implied.
+    */
    UNSPECIFIED;
    
+   /**
+    * @return the Context object for this scope
+    */
    public Context getContext() {
       switch (this)
       {
