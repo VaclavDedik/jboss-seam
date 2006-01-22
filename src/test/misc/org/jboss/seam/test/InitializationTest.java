d2fb7239b5f1a2a8469ea651cf4fd3c5cd85a141
Gavin King <gavin.king@gmail.com>
2006-01-22 00:41:08 +0000

    fixed tests

--- src/test/misc/org/jboss/seam/test/InitializationTest.java
+++ src/test/misc/org/jboss/seam/test/InitializationTest.java
@@ -14,7 +14,7 @@ public class InitializationTest
    {
       MockServletContext servletContext = new MockServletContext();
       new Initialization(servletContext).setScannerEnabled(false).init();
-      assert servletContext.getAttributes().size()==9 + 2*8;
+      assert servletContext.getAttributes().size()==11 + 2*8;
       assert !Contexts.isApplicationContextActive();
    }
 
@@ -25,7 +25,7 @@ public class InitializationTest
       servletContext.getInitParameters().put(Init.COMPONENT_CLASSES, "org.jboss.seam.test.Foo, org.jboss.seam.test.Bar");
       servletContext.getInitParameters().put(Init.MANAGED_PERSISTENCE_CONTEXTS, "bookingDatabase");
       new Initialization(servletContext).setScannerEnabled(false).init();
-      assert servletContext.getAttributes().size()==12 + 2*8;
+      assert servletContext.getAttributes().size()==14 + 2*8;
       assert !Contexts.isApplicationContextActive();
    }
 }
