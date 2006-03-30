f854034c0d897fdd2737e7d2dd6640b894717a23
Gavin King <gavin.king@gmail.com>
2006-03-30 11:46:30 +0000

    added s:action and made work in facelets

---
+++ src/ui/org/jboss/seam/ui/UIAction.java
@@ -0,0 +1,68 @@
+package org.jboss.seam.ui;
+
+import javax.faces.component.UIParameter;
+import javax.faces.context.FacesContext;
+
+public class UIAction extends UIParameter
+{
+   
+   public static final String COMPONENT_TYPE = "org.jboss.seam.ui.UIAction";
+   
+   private String outcome;
+   
+   public void setAction(String action)
+   {
+      this.outcome = action;
+   }
+   
+   public String getAction()
+   {
+      return outcome;
+   }
+   
+   private boolean isMethodBinding()
+   {
+      return outcome==null;
+   }
+
+   private String getMethodBindingExpression()
+   {
+      return getValueBinding("action").getExpressionString();
+   }
+
+   @Override
+   public String getName()
+   {
+      return isMethodBinding() ? "actionMethod" : "actionOutcome";
+   }
+   
+   @Override
+   public Object getValue()
+   {
+      if ( isMethodBinding() )
+      {
+         String expression = getMethodBindingExpression();
+         return expression.substring( 2, expression.length()-1 ); 
+      }
+      else
+      {
+         return outcome;
+      }
+   }
+   
+   @Override
+   public void restoreState(FacesContext context, Object state) {
+      Object[] values = (Object[]) state;
+      super.restoreState(context, values[0]);
+      outcome = (String) values[1];
+   }
+
+   @Override
+   public Object saveState(FacesContext context) {
+      Object[] values = new Object[2];
+      values[0] = super.saveState(context);
+      values[1] = outcome;
+      return values;
+   }
+
+}
