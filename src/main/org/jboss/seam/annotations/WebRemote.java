0c4b2df6a2804c3dd6bf6a05c8c3703e177365c0
Gavin King <gavin.king@gmail.com>
2006-04-18 22:04:29 +0000

    documented

--- src/main/org/jboss/seam/annotations/WebRemote.java
+++ src/main/org/jboss/seam/annotations/WebRemote.java
@@ -2,6 +2,8 @@ package org.jboss.seam.annotations;
 
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
@@ -11,6 +13,7 @@ import java.lang.annotation.Target;
  * @author Shane Bryzak
  */
 @Target(METHOD)
+@Documented
 @Retention(RUNTIME)
 public @interface WebRemote {
 }
