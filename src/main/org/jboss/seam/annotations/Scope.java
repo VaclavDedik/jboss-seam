a820ea5e34b09593a3b37fa703932625c5226c3c
Gavin King <gavin.king@gmail.com>
2006-04-24 00:24:21 +0000

    @Inherited

--- src/main/org/jboss/seam/annotations/Scope.java
+++ src/main/org/jboss/seam/annotations/Scope.java
@@ -10,6 +10,7 @@ import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 import java.lang.annotation.Documented;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
@@ -24,6 +25,7 @@ import org.jboss.seam.ScopeType;
 @Target(TYPE)
 @Retention(RUNTIME)
 @Documented
+@Inherited
 public @interface Scope {
    ScopeType value();
 }
--- src/main/org/jboss/seam/annotations/Transactional.java
+++ src/main/org/jboss/seam/annotations/Transactional.java
@@ -5,6 +5,7 @@ import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 import java.lang.annotation.Documented;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
@@ -20,6 +21,7 @@ import java.lang.annotation.Target;
 @Target({TYPE, METHOD})
 @Retention(RUNTIME)
 @Documented
+@Inherited
 public @interface Transactional
 {
 
