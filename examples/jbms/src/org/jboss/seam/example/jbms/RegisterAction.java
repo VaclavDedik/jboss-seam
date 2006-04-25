25fc29899be6cfe3bc42c73f9241c025f553c090
Gavin King <gavin.king@gmail.com>
2006-04-25 05:54:54 +0000

    first part of mail example

---
+++ examples/jbms/src/org/jboss/seam/example/jbms/RegisterAction.java
@@ -0,0 +1,49 @@
+//$Id$
+package org.jboss.seam.example.jbms;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.hibernate.validator.Valid;
+import org.jboss.seam.annotations.IfInvalid;
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Outcome;
+import org.jboss.seam.core.FacesMessages;
+
+@Stateless
+@Name("register")
+public class RegisterAction implements Register
+{
+
+   @In @Valid
+   private User user;
+   
+   @PersistenceContext
+   private EntityManager em;
+   
+   @IfInvalid(outcome=Outcome.REDISPLAY)
+   public String register()
+   {
+      List existing = em.createQuery("select username from User where username=:username")
+         .setParameter("username", user.getUsername())
+         .getResultList();
+      if (existing.size()==0)
+      {
+         em.persist(user);
+         
+         //TODO: send the confirmation email!!
+         
+         return "success";
+      }
+      else
+      {
+         FacesMessages.instance().add("User #{user.username} already exists");
+         return null;
+      }
+   }
+
+}
---
+++ examples/jbms/view/register.jsp
@@ -0,0 +1,33 @@
+<%@ taglib uri="http://java.sun.com/jsf/html" prefix="h" %>
+<%@ taglib uri="http://java.sun.com/jsf/core" prefix="f" %>
+<html>
+ <head>
+  <title>Register New User</title>
+ </head>
+ <body>
+  <f:view>
+   <h:form>
+     <table border="0">
+       <tr>
+         <td>Username</td>
+         <td><h:inputText value="#{user.username}"/></td>
+       </tr>
+       <tr>
+         <td>Real Name</td>
+         <td><h:inputText value="#{user.name}"/></td>
+       </tr>
+       <tr>
+         <td>Email</td>
+         <td><h:inputText value="#{user.email}"/></td>
+       </tr>
+       <tr>
+         <td>Password</td>
+         <td><h:inputSecret value="#{user.password}"/></td>
+       </tr>
+     </table>
+     <h:messages/>
+     <h:commandButton type="submit" value="Register" action="#{register.register}"/>
+   </h:form>
+  </f:view>
+ </body>
+</html>
