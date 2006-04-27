cf4f00d44737128d10094593cfaf90aaf1054770
Gavin King <gavin.king@gmail.com>
2006-04-27 20:50:33 +0000

    refactor to static

---
+++ src/main/org/jboss/seam/util/Validation.java
@@ -0,0 +1,29 @@
+package org.jboss.seam.util;
+
+import org.hibernate.validator.ClassValidator;
+import org.jboss.seam.Component;
+import org.jboss.seam.Seam;
+import org.jboss.seam.core.ResourceBundle;
+
+public class Validation
+{
+
+   public static ClassValidator getValidator(Class modelClass)
+   {
+      ClassValidator validator;
+      String componentName = Seam.getComponentName(modelClass);
+      if (componentName==null)
+      {
+         java.util.ResourceBundle bundle = ResourceBundle.instance();
+         validator = bundle==null ? 
+               new ClassValidator(modelClass) : 
+               new ClassValidator(modelClass, bundle);
+      }
+      else
+      {
+         validator = Component.forName(componentName).getValidator();
+      }
+      return validator;
+   }
+
+}
