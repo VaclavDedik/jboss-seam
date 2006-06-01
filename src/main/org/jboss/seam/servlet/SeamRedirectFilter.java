275940eecff1929cd872cac8c4f81c1ead999bcd
Gavin King <gavin.king@gmail.com>
2006-06-01 17:36:13 +0000

    use a HttpServletResponseWrapper, as suggested by Roger Kitain

--- src/main/org/jboss/seam/servlet/SeamRedirectFilter.java
+++ src/main/org/jboss/seam/servlet/SeamRedirectFilter.java
@@ -8,19 +8,45 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponseWrapper;
+
+import org.jboss.seam.contexts.Contexts;
+import org.jboss.seam.core.Manager;
 
 /**
  * Propagates the conversation context across a browser redirect
  * 
  * @author Gavin King
  */
-public class SeamRedirectFilter implements Filter {
+public class SeamRedirectFilter implements Filter 
+{
 
    public void init(FilterConfig config) throws ServletException {}
 
    public void doFilter(ServletRequest request, ServletResponse response,
-         FilterChain chain) throws IOException, ServletException {
-      chain.doFilter( request, ResponseInvocationHandler.proxyResponse(response) );
+         FilterChain chain) throws IOException, ServletException 
+   {
+      chain.doFilter( request, wrapResponse( (HttpServletResponse) response ) );
+   }
+   
+   private static ServletResponse wrapResponse(HttpServletResponse response) 
+   {
+      return new HttpServletResponseWrapper(response)
+      {
+         @Override
+         public void sendRedirect(String url) throws IOException
+         {
+            if ( Contexts.isEventContextActive() )
+            {
+               if ( !url.contains("?conversationId=") )
+               {
+                  url = Manager.instance().encodeConversationId(url);
+               }
+            }
+            super.sendRedirect(url);
+         }  
+      };
    }
 
    public void destroy() {}
