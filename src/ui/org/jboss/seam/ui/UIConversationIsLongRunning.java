5117c03801cfde29c4a0fdf2cfbc285cc027072f
Gavin King <gavin.king@gmail.com>
2006-06-30 18:56:01 +0000

    replacement approach for @Conversational

---
+++ src/ui/org/jboss/seam/ui/UIConversationIsLongRunning.java
@@ -0,0 +1,24 @@
+package org.jboss.seam.ui;
+
+import javax.faces.component.UIParameter;
+
+import org.jboss.seam.core.Manager;
+
+public class UIConversationIsLongRunning extends UIParameter
+{
+   
+   public static final String COMPONENT_TYPE = "org.jboss.seam.ui.UIConversationIsLongRunning";
+   
+   @Override
+   public String getName()
+   {
+      return "conversationIsLongRunning";
+   }
+   
+   @Override
+   public Object getValue()
+   {
+      return Manager.instance().isReallyLongRunningConversation();
+   }
+
+}
