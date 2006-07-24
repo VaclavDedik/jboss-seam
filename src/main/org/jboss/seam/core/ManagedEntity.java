8f3157b53ab1ebac524b1a7d1af1eb06f5451c4a
Gavin King <gavin.king@gmail.com>
2006-07-24 20:44:51 +0000

    handle nonstring id types more elegantly

--- src/main/org/jboss/seam/core/ManagedEntity.java
+++ src/main/org/jboss/seam/core/ManagedEntity.java
@@ -2,6 +2,8 @@ package org.jboss.seam.core;
 
 import static org.jboss.seam.InterceptionType.NEVER;
 
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
 import javax.persistence.EntityManager;
 
 import org.jboss.seam.annotations.Intercept;
@@ -13,6 +15,9 @@ public class ManagedEntity
    private EntityManager entityManager;
    private Object id;
    private String entityClass;
+   private Object newInstance;
+   private String converterId;
+   private Converter converter;
    
    public EntityManager getEntityManager()
    {
@@ -45,10 +50,68 @@ public class ManagedEntity
    }
 
    @Unwrap
-   public Object getInstance() throws ClassNotFoundException
+   public Object getInstance() throws Exception
    {
       Class<?> clazz = Class.forName(entityClass);
-      return entityManager.find(clazz, id);
+      if (id==null)
+      {
+         if (newInstance==null)
+         {
+            newInstance = clazz.newInstance();
+         }
+         return newInstance;
+      }
+      else
+      {
+         return entityManager.find( clazz, getConvertedId() );
+      }
    }
+   
+   //////////// TODO: copy/paste from ManagedHibernateEntity ///////////////////
+   
+   private Object getConvertedId()
+   {
+      FacesContext facesContext = FacesContext.getCurrentInstance();
+      if (converterId!=null)
+      {
+         converter = facesContext.getApplication().createConverter(converterId); //cache the lookup
+      }
+      
+      if (converter==null)
+      {
+         //TODO: look for an @Id annotation and guess the id type!
+         //converter = facesContext.getApplication().createConverter(idClass)
+         return id;
+      }
+      else
+      {
+         return converter.getAsObject( 
+               facesContext, 
+               facesContext.getViewRoot(), 
+               (String) id 
+            );
+      }
+   }
+
+   public String getConverterId()
+   {
+      return converterId;
+   }
+
+   public void setConverterId(String converterId)
+   {
+      this.converterId = converterId;
+   }
+
+   public Converter getConverter()
+   {
+      return converter;
+   }
+
+   public void setConverter(Converter converter)
+   {
+      this.converter = converter;
+   }
+
 
 }
--- src/main/org/jboss/seam/core/ManagedHibernateEntity.java
+++ src/main/org/jboss/seam/core/ManagedHibernateEntity.java
@@ -2,6 +2,9 @@ package org.jboss.seam.core;
 
 import java.io.Serializable;
 
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+
 import org.hibernate.Session;
 import org.jboss.seam.annotations.Transactional;
 import org.jboss.seam.annotations.Unwrap;
@@ -11,6 +14,9 @@ public class ManagedHibernateEntity
    private Session session;
    private Serializable id;
    private String entityClass;
+   private Object newInstance;
+   private String converterId;
+   private Converter converter;
    
    public Session getSession()
    {
@@ -43,10 +49,67 @@ public class ManagedHibernateEntity
    }
 
    @Unwrap @Transactional
-   public Object getInstance() throws ClassNotFoundException
+   public Object getInstance() throws Exception
    {
       Class clazz = Class.forName(entityClass);
-      return session.get(clazz, id);
+      if (id==null)
+      {
+         if (newInstance==null)
+         {
+            newInstance = clazz.newInstance();
+         }
+         return newInstance;
+      }
+      else
+      {
+         return session.get( clazz, getConvertedId() );
+      }
+   }
+   
+   ////////////TODO: copy/paste from ManagedEntity ///////////////////
+
+   private Serializable getConvertedId()
+   {
+      FacesContext facesContext = FacesContext.getCurrentInstance();
+      if (converterId!=null)
+      {
+         converter = facesContext.getApplication().createConverter(converterId); //cache the lookup
+      }
+      
+      if (converter==null)
+      {
+         //TODO: look for an @Id annotation and guess the id type!
+         //converter = facesContext.getApplication().createConverter(idClass)
+         return id;
+      }
+      else
+      {
+         return (Serializable) converter.getAsObject( 
+               facesContext, 
+               facesContext.getViewRoot(), 
+               (String) id 
+            );
+      }
+   }
+
+   public String getConverterId()
+   {
+      return converterId;
+   }
+
+   public void setConverterId(String converterId)
+   {
+      this.converterId = converterId;
+   }
+
+   public Converter getConverter()
+   {
+      return converter;
+   }
+
+   public void setConverter(Converter converter)
+   {
+      this.converter = converter;
    }
 
 }
