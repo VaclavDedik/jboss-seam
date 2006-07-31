476091e8983c0a621d340cd0f80a4895092d24cd
Shane Bryzak <sbryzak@redhat.com>
2006-07-31 03:22:23 +0000

    Re-added Authenticator interface

---
+++ src/main/org/jboss/seam/security/authenticator/Authenticator.java
@@ -0,0 +1,15 @@
+package org.jboss.seam.security.authenticator;
+
+import org.jboss.seam.security.AuthenticationException;
+import org.jboss.seam.security.Authentication;
+
+/**
+ *
+ *
+ * @author Shane Bryzak
+ */
+public interface Authenticator
+{
+  Authentication authenticate(Authentication authentication)
+      throws AuthenticationException;
+}
