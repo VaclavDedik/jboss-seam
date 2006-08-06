e05bc656976fa8e7aad33d3dacbaa9e06e602630
Shane Bryzak <sbryzak@redhat.com>
2006-08-06 13:59:21 +0000

    Cleaned up remoting documentation and examples

--- examples/remoting/helloworld/src/org/jboss/seam/example/remoting/HelloAction.java
+++ examples/remoting/helloworld/src/org/jboss/seam/example/remoting/HelloAction.java
@@ -1,14 +1,11 @@
 package org.jboss.seam.example.remoting;
 
 import javax.ejb.Stateless;
-import javax.interceptor.Interceptors;
 
 import org.jboss.seam.annotations.Name;
-import org.jboss.seam.ejb.SeamInterceptor;
 
 @Stateless
 @Name("helloAction")
-@Interceptors(SeamInterceptor.class)
 public class HelloAction implements HelloLocal {
   public String sayHello(String name) {
     return "Hello, " + name;
--- examples/remoting/progressbar/src/org/jboss/seam/example/remoting/ProgressBarAction.java
+++ examples/remoting/progressbar/src/org/jboss/seam/example/remoting/ProgressBarAction.java
@@ -3,15 +3,12 @@ package org.jboss.seam.example.remoting;
 import java.util.Random;
 
 import javax.ejb.Stateless;
-import javax.interceptor.Interceptors;
 
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
-import org.jboss.seam.ejb.SeamInterceptor;
 
 @Stateless
 @Name("progressBarAction")
-@Interceptors(SeamInterceptor.class)
 public class ProgressBarAction implements ProgressBarLocal {
 
   @In(create = true)
