174bd3d0043f56cd3c1cbb93c4e190963d258d15
Shane Bryzak <sbryzak@redhat.com>
2006-08-13 11:06:48 +0000

    Fixed test

--- src/test/misc/org/jboss/seam/test/InitializationTest.java
+++ src/test/misc/org/jboss/seam/test/InitializationTest.java
@@ -1,22 +1,44 @@
 //$Id$
+
 package org.jboss.seam.test;
 
+
+
 import org.jboss.seam.contexts.Contexts;
+
 import org.jboss.seam.init.Initialization;
+
 import org.jboss.seam.mock.MockServletContext;
+
 import org.testng.annotations.Test;
 
+
+
 public class InitializationTest
+
 {
+
    @Test
+
    public void testEmptyInitialization()
+
    {
+
       MockServletContext servletContext = new MockServletContext();
+
       new Initialization(servletContext).setScannerEnabled(false).init();
-      assert servletContext.getAttributes().size()==24 + 2*10;
+
+      assert servletContext.getAttributes().size()==25 + 2*10;
+
       assert !Contexts.isApplicationContextActive();
+
    }
-   
+
+
+
    //TODO: write a test for components.xml
-   
+
+
+
 }
+
