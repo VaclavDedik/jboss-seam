f2e15e51b906c8f4f0467eeddab4d5108e3a5fc4
Shane Bryzak <sbryzak@redhat.com>
2006-08-23 14:03:49 +0000

    Added demo account details to home page

--- examples/security/resources/import.sql
+++ examples/security/resources/import.sql
@@ -3,3 +3,4 @@ insert into User (username, password) values ('demo', 'demo')
 insert into Role (roleid, username, role) values (1, 'shane', 'user');
 insert into Role (roleid, username, role) values (2, 'shane', 'admin');
 insert into Role (roleid, username, role) values (3, 'demo', 'user');
+insert into Role (roleid, username, role) values (4, 'demo', 'admin');
