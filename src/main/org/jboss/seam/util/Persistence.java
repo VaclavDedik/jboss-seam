646c63013fc4a323114fa42a8f6b9f4610b6f137
Gavin King <gavin.king@gmail.com>
2006-09-23 16:30:11 +0000

    use Hibernate isDirty method

--- src/main/org/jboss/seam/util/Persistence.java
+++ src/main/org/jboss/seam/util/Persistence.java
@@ -10,7 +10,26 @@ public class Persistence
 
    public static void setFlushModeManual(EntityManager entityManager)
    {
-      ( (Session) entityManager.getDelegate() ).setFlushMode(FlushMode.NEVER);
+      if (entityManager instanceof Session)
+      {
+         ( (Session) entityManager.getDelegate() ).setFlushMode(FlushMode.NEVER);
+      }
+      else
+      {
+         throw new IllegalArgumentException("FlusMode.MANUAL only supported for Hibernate EntityManager");
+      }
+   }
+
+   public static boolean isDirty(EntityManager entityManager)
+   {
+      if (entityManager instanceof Session)
+      {
+         return ( (Session) entityManager.getDelegate() ).isDirty();
+      }
+      else
+      {
+         return true; //best we can do!
+      }
    }
 
 }
