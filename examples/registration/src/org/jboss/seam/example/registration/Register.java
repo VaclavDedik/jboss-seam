a72f48cb1101d01bd75f63f70bd4763bfe095c9b
Gavin King <gavin.king@gmail.com>
2006-09-28 00:36:56 +0000

    this is the first example so it must be SIMPLE

--- examples/registration/src/org/jboss/seam/example/registration/Register.java
+++ examples/registration/src/org/jboss/seam/example/registration/Register.java
@@ -2,12 +2,9 @@
 package org.jboss.seam.example.registration;
 
 import javax.ejb.Local;
-import javax.faces.context.FacesContext;
 
 @Local
 public interface Register
 {
-   public void logClientIP(FacesContext facesContext);
-
    public String register();
 }
--- examples/registration/src/org/jboss/seam/example/registration/RegisterAction.java
+++ examples/registration/src/org/jboss/seam/example/registration/RegisterAction.java
@@ -4,11 +4,8 @@ package org.jboss.seam.example.registration;
 import java.util.List;
 
 import javax.ejb.Stateless;
-import javax.faces.context.ExternalContext;
-import javax.faces.context.FacesContext;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.servlet.ServletRequest;
 
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Logger;
@@ -30,25 +27,6 @@ public class RegisterAction implements Register
    @Logger
    private Log log;
    
-   /**
-    * This demonstrates passing a parameter to an action from 
-    * pages.xml.  In this case, we call this method using
-    * <page view-id="/registered.jsp" 
-    *       action="#{register.logClientIP(facesContext)}" /> 
-    *
-    * Note that facesContext is a reserved word that always
-    * binds to the current FacesContext instance.
-    */
-   public void logClientIP(FacesContext facesContext)
-   {
-      ExternalContext extCtx = facesContext.getExternalContext();
-      if (extCtx.getRequest() instanceof ServletRequest)
-      {
-         ServletRequest request = (ServletRequest)extCtx.getRequest();
-         log.info("Registered user from IP: " + request.getRemoteAddr());
-      }
-   }
-
    public String register()
    {
       List existing = em.createQuery("select username from User where username=:username")
