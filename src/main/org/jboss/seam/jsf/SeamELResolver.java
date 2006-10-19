86eb2d4aeefd5c8627ac0438be1779a54cac9d10
Gavin King <gavin.king@gmail.com>
2006-10-19 22:51:17 +0000

    JSF 1.2 style variable resolution

---
+++ src/main/org/jboss/seam/jsf/SeamELResolver.java
@@ -0,0 +1,55 @@
+package org.jboss.seam.jsf;
+
+import java.util.Iterator;
+
+import javax.el.ELContext;
+import javax.el.ELResolver;
+
+import org.jboss.seam.Component;
+
+public class SeamELResolver extends ELResolver
+{
+
+   @Override
+   public Class getCommonPropertyType(ELContext context, Object base)
+   {
+      return null;
+   }
+
+   @Override
+   public Iterator getFeatureDescriptors(ELContext context, Object base)
+   {
+      return null;
+   }
+
+   @Override
+   public Class getType(ELContext context, Object base, Object property)
+   {
+      return null;
+   }
+
+   @Override
+   public Object getValue(ELContext context, Object base, Object property)
+   {
+      if (base==null)
+      {
+         String name = (String) property;
+         name = name.replace('$', '.');
+         return Component.getInstance(name);
+      }
+      else
+      {
+         return null;
+      }
+   }
+
+   @Override
+   public boolean isReadOnly(ELContext context, Object base, Object property)
+   {
+      return false;
+   }
+
+   @Override
+   public void setValue(ELContext context, Object base, Object property, Object value) {}
+
+}
