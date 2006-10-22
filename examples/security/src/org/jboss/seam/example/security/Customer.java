2d720252401dfc2098b1c6bf625d6eb9d0746c71
Shane Bryzak <sbryzak@redhat.com>
2006-10-22 10:01:02 +0000

    added classes to demo object acl's

---
+++ examples/security/src/org/jboss/seam/example/security/Customer.java
@@ -0,0 +1,42 @@
+package org.jboss.seam.example.security;
+
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.DefinePermissions;
+import org.jboss.seam.annotations.AclProvider;
+
+/**
+ * <p>PROPRIETARY/CONFIDENTIAL Use of this product is subject to license terms.
+ * Copyright (c) 2006 Symantec Corporation. All rights reserved.</p>
+ *
+ * @author Shane Bryzak
+ * @version 1.0
+ */
+@Name("customer")
+@DefinePermissions(permissions = {
+  @AclProvider(actions = "modify", provider = "customerAclProvider")
+})
+public class Customer
+{
+  private String name;
+  private boolean readonly;
+
+  public String getName()
+  {
+    return name;
+  }
+
+  public void setName(String name)
+  {
+    this.name = name;
+  }
+
+  public boolean isReadonly()
+  {
+    return readonly;
+  }
+
+  public void setReadonly(boolean readonly)
+  {
+    this.readonly = readonly;
+  }
+}
