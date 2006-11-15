77d7a018cbb01bb756af31f1cf284ff52fb8d440
Norman Richards <orb@jump.net>
2006-11-15 19:57:16 +0000

    use namespaced xml

--- examples/seampay/src/org/jboss/seam/example/seampay/PaymentController.java
+++ examples/seampay/src/org/jboss/seam/example/seampay/PaymentController.java
@@ -5,12 +5,13 @@ import javax.ejb.TimerHandle;
 
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Logger;
+import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.RequestParameter;
 import org.jboss.seam.annotations.Transactional;
 import org.jboss.seam.framework.EntityHome;
 import org.jboss.seam.log.Log;
 
-
+@Name("paymentHome")
 public class PaymentController 
     extends EntityHome<Payment>
 {
