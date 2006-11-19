a1850d5a0c9b396640e34977200dd02086752c77
Gavin King <gavin.king@gmail.com>
2006-11-19 19:18:27 +0000

    ns

--- src/main/org/jboss/seam/theme/package-info.java
+++ src/main/org/jboss/seam/theme/package-info.java
@@ -1,4 +1,4 @@
-@Namespace("http://jboss.com/products/seam/theme")
+@Namespace(value="http://jboss.com/products/seam/theme", prefix="org.jboss.seam.theme")
 package org.jboss.seam.theme;
 
 import org.jboss.seam.annotations.Namespace;
