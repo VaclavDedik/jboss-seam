2ae43d9c52acec5e7b8ee9b2204af7234fb53e3d
Gavin King <gavin.king@gmail.com>
2006-11-23 21:26:44 +0000

    workaround for RI's verifyObjects stuff

--- src/ui/org/jboss/seam/ui/ConvertDateTime.java
+++ src/ui/org/jboss/seam/ui/ConvertDateTime.java
@@ -4,18 +4,27 @@ import java.util.TimeZone;
 
 import javax.faces.convert.DateTimeConverter;
 
+import org.jboss.seam.contexts.Contexts;
+
 public class ConvertDateTime extends DateTimeConverter
 {
 
    public ConvertDateTime()
    {
-      setTimeZone( org.jboss.seam.core.TimeZone.instance() );
+      setTimeZone( getTimeZone() );
    }
 
    @Override
    public TimeZone getTimeZone()
    {
-      return org.jboss.seam.core.TimeZone.instance();
+      if ( Contexts.isApplicationContextActive() )
+      {
+         return org.jboss.seam.core.TimeZone.instance();
+      }
+      else
+      {
+         return TimeZone.getDefault();
+      }
    }
 
 }
