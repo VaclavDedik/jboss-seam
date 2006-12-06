0ed567776606a95a312a670e18e87213d80a54ce
Gavin King <gavin.king@gmail.com>
2006-12-06 03:21:03 +0000

    get rid of broken example

--- examples/remoting/stories/resources/import.sql
+++
@@ -1,3 +0,0 @@
-insert into project (id, name) values(1, 'the project')
-insert into story (id, name, points, body, posn, project_id) values (1, 'do something', 2, 'blah blah blah', 0, 1)
-insert into story (id, name, points, body, posn, project_id) values (2, 'do something else', 1, 'blah blah blah', 1, 1)
