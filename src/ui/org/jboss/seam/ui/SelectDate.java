15d0fb47de007b598a764ececf833e65d7107240
Norman Richards <orb@jump.net>
2006-12-07 00:19:15 +0000

    JBSEAM-556: add s:selectDate

---
+++ src/ui/org/jboss/seam/ui/SelectDate.java
@@ -0,0 +1,133 @@
+package org.jboss.seam.ui;
+
+import java.io.*;
+import java.util.*;
+
+import javax.faces.application.*;
+import javax.faces.component.*;
+import javax.faces.context.*;
+import javax.faces.el.ValueBinding;
+
+public class SelectDate 
+    extends UIComponentBase
+{
+    public static final String COMPONENT_TYPE   = "org.jboss.seam.ui.SelectDate";
+    public static final String COMPONENT_FAMILY = "org.jboss.seam.ui.SelectDate";
+    
+    public static final String SELECTDATE_JS    = "org/jboss/seam/ui/selectDate.js";
+
+    private String info;
+    private String forField;
+    
+    
+    public String getFor()
+    {
+        return forField;
+    }
+    
+    public void setFor(String forField)
+    {
+        this.forField = forField;
+    }    
+
+    
+    public String getInfo()
+    {
+        return info;
+    }
+    
+    public void setInfo(String info)
+    {
+        this.info = info;
+    }    
+
+    @Override
+    public String getFamily()
+    {
+        return COMPONENT_FAMILY;
+    }
+    
+    @Override
+    public boolean getRendersChildren() {
+        return false;
+    }
+
+
+   @Override
+   public void restoreState(FacesContext context, Object state) {
+      Object[] values = (Object[]) state;
+      super.restoreState(context, values[0]);
+      info = (String) values[1];
+      forField = (String) values[2];
+   }
+
+   @Override
+   public Object saveState(FacesContext context) {
+      Object[] values = new Object[3];
+      values[0] = super.saveState(context);
+      values[1] = info;
+      values[2] = forField;
+      return values;
+   }
+    
+    @Override
+    public void encodeBegin(FacesContext context) throws IOException
+    {
+        UIComponent forComponent = findComponent(forField);
+
+        writeScript(context, SELECTDATE_JS);
+        ResponseWriter response = context.getResponseWriter();
+        response.startElement("span", this);
+        response.writeAttribute("onclick", 
+                                "displayDatePicker('" + forComponent.getClientId(context) + "');", 
+                                null);
+    }
+
+    @Override
+    public void encodeEnd(FacesContext context) throws IOException
+    {
+        ResponseWriter response = context.getResponseWriter();
+        response.endElement("span");
+        response.flush();
+        super.encodeEnd(context);
+    }
+
+
+    public void writeScript(FacesContext context,
+                            String scriptResource) 
+        throws IOException
+    {
+        Map request = context.getExternalContext().getRequestMap();
+        if (request.get("SELECTDATE_SCRIPT") != null) {
+            // already written out
+            return;
+        }
+            
+        request.put("SELECTDATE_SCRIPT", scriptResource);
+        
+        ViewHandler handler = context.getApplication().getViewHandler();
+
+        ResponseWriter response = context.getResponseWriter();
+        response.startElement("script", null);
+        response.writeAttribute("type", "text/javascript", null);
+        InputStream is = this.getClass().getClassLoader().getResourceAsStream(scriptResource);
+
+        if (is == null) {
+            throw new IOException("resource " + scriptResource + " not found");
+        }
+
+        try {
+            InputStreamReader reader = new InputStreamReader(is);
+            char[] buf = new char[512];
+            int nread; 
+            while ((nread = reader.read(buf,0,512))!= -1) {
+                response.write(buf, 0, nread);
+            }
+        } finally {
+            is.close();
+        }
+
+        response.endElement("script");
+    }
+                            
+}
---
+++ src/ui/org/jboss/seam/ui/tag/SelectDateTag.java
@@ -0,0 +1,58 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2006, JBoss Inc., and individual contributors as indicated
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.seam.ui.tag;
+
+import javax.faces.component.UIComponent;
+
+import org.jboss.seam.ui.SelectDate;
+
+
+public class SelectDateTag extends UIComponentTagBase
+{
+    @Override
+    public String getComponentType()
+    {
+        return SelectDate.COMPONENT_TYPE;
+    }
+
+    @Override
+    public String getRendererType()
+    {
+        return null;
+    }
+
+    private String forField;
+
+    @Override
+    protected void setProperties(UIComponent component)
+    {
+        super.setProperties(component);
+        setStringProperty(component, "for", forField);
+    }
+
+    public void setFor(String forField)
+    {
+        this.forField = forField;
+    }
+
+}
