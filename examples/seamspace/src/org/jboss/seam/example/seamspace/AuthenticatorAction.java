7c8f01543f13811a0471693605823d4b05dc8eaa
Shane Bryzak <sbryzak@redhat.com>
2006-12-31 01:26:42 +0000

    use login module instead of auth provider

--- examples/seamspace/src/org/jboss/seam/example/seamspace/AuthenticatorAction.java
+++ examples/seamspace/src/org/jboss/seam/example/seamspace/AuthenticatorAction.java
@@ -1,14 +1,17 @@
 package org.jboss.seam.example.seamspace;
 
+import java.util.Map;
+
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
+import javax.security.auth.Subject;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.login.LoginException;
+import javax.security.auth.spi.LoginModule;
 
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
-import org.jboss.seam.security.Identity;
 import org.jboss.seam.security.AuthenticationException;
-import org.jboss.seam.security.UsernamePasswordToken;
-import org.jboss.seam.security.provider.AuthenticationProvider;
 import org.jboss.seam.security.Role;
 
 /**
@@ -17,37 +20,63 @@ import org.jboss.seam.security.Role;
  * @author Shane Bryzak
  */
 @Name("authenticatorAction")
-public class AuthenticatorAction implements AuthenticationProvider
+public class AuthenticatorAction implements LoginModule
 {
-  @In(create=true)
-    private EntityManager entityManager;
+   private Subject subject;
+   
+   @In(create=true)
+       private EntityManager entityManager;   
+   
+  public boolean abort() throws LoginException
+   {
+      // TODO Auto-generated method stub
+      return false;
+   }
 
-  public Identity authenticate(Identity authentication)
-      throws AuthenticationException
-  {
-    try
-    {
-      Member member = (Member) entityManager.createQuery(
-          "from Member where username = :username and password = :password")
-          .setParameter("username", authentication.getPrincipal().toString())
-          .setParameter("password", authentication.getCredentials())
-          .getSingleResult();
+   public boolean commit() throws LoginException
+   {
+      // TODO Auto-generated method stub
+      return false;
+   }
 
-      Role[] roles = new Role[member.getRoles().size()];
-      int idx = 0;
-      for (MemberRole mr : member.getRoles())
-        roles[idx++] = new Role(mr.getName());
+   public void initialize(Subject subject, CallbackHandler callbackHandler, 
+         Map<String, ?> sharedState, Map<String, ?> options)
+   {
+      this.subject = subject;     
+   }
 
-      return new UsernamePasswordToken(authentication.getPrincipal(),
-                                       authentication.getCredentials(), roles);
-    }
-    catch (NoResultException ex)
-    {
-      throw new AuthenticationException("Invalid username/password");
-    }
-    catch (Exception ex)
-    {
-      throw new AuthenticationException("Unknown authentication error", ex);
-    }
-  }
+   public boolean login() throws LoginException
+   {
+      try
+      {         
+        Member member = (Member) entityManager.createQuery(
+            "from Member where username = :username and password = :password")
+//            .setParameter("username", auth.getPrincipal().toString())
+//            .setParameter("password", auth.getCredentials())
+            .getSingleResult();
+
+        Role[] roles = new Role[member.getRoles().size()];
+        int idx = 0;
+        for (MemberRole mr : member.getRoles())
+          roles[idx++] = new Role(mr.getName());
+        
+//        return new UsernamePasswordToken(authentication.getPrincipal(),
+//                                         authentication.getCredentials(), roles);
+        return true;
+      }
+      catch (NoResultException ex)
+      {
+        throw new AuthenticationException("Invalid username/password");
+      }
+      catch (Exception ex)
+      {
+        throw new AuthenticationException("Unknown authentication error", ex);
+      }
+   }
+
+   public boolean logout() throws LoginException
+   {
+      // TODO Auto-generated method stub
+      return false;
+   }
 }
