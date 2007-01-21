ec16f388d7d1a64a7e37e106aa496f4329638c22
Pete Muir <pmuir@bleepbleep.org.uk>
2007-01-21 14:41:55 +0000

    JBSEAM-626 Example for mail support

---
+++ examples/mail/src/org/jboss/seam/example/mail/Person.java
@@ -0,0 +1,51 @@
+package org.jboss.seam.example.mail;
+
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.Begin;
+import org.jboss.seam.annotations.Create;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+
+@Name("person")
+@Scope(ScopeType.CONVERSATION)
+public class Person
+{
+   private String firstname;
+   private String lastname;
+   private String address;
+   
+   @Create
+   @Begin(join=true)
+   public void create() 
+   {
+      
+   }
+   
+   public String getAddress()
+   {
+      return address;
+   }
+   public void setAddress(String address)
+   {
+      this.address = address;
+   }
+   public String getFirstname()
+   {
+      return firstname;
+   }
+   public void setFirstname(String firstname)
+   {
+      this.firstname = firstname;
+   }
+   public String getLastname()
+   {
+      return lastname;
+   }
+   public void setLastname(String lastname)
+   {
+      this.lastname = lastname;
+   }
+   
+   
+   
+}
