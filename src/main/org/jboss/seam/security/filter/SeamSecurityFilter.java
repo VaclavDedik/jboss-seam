df622cad5f37e89e6e5a0e33d88f1df8a60b3d59
Shane Bryzak <sbryzak@redhat.com>
2007-01-24 22:46:54 +0000

    only proceed down the chain if not redirecting

--- src/main/org/jboss/seam/security/filter/SeamSecurityFilter.java
+++ src/main/org/jboss/seam/security/filter/SeamSecurityFilter.java
@@ -24,90 +24,93 @@ import org.jboss.seam.security.config.SecurityConstraint;
 
 /**
  * A servlet filter that performs authentication within a Seam application.
- *
+ * 
  * @author Shane Bryzak
  */
 public class SeamSecurityFilter implements Filter
 {
-  private SecurityConfiguration config;
+   private SecurityConfiguration config;
 
-  private ServletContext servletContext;
+   private ServletContext servletContext;
 
-  public void init(FilterConfig filterConfig)
-      throws ServletException
-  {
-    servletContext = filterConfig.getServletContext();
-    WebApplicationContext ctx = new WebApplicationContext(servletContext);
-    config = (SecurityConfiguration) ctx.get(SecurityConfiguration.class);
-  }
+   public void init(FilterConfig filterConfig) throws ServletException
+   {
+      servletContext = filterConfig.getServletContext();
+      WebApplicationContext ctx = new WebApplicationContext(servletContext);
+      config = (SecurityConfiguration) ctx.get(SecurityConfiguration.class);
+   }
 
-  /**
-   *
-   * @param request ServletRequest
-   * @param response ServletResponse
-   * @param chain FilterChain
-   * @throws IOException
-   * @throws ServletException
-   */
-  public void doFilter(ServletRequest request, ServletResponse response,
-                       FilterChain chain)
-      throws IOException, ServletException
-  {
-    HttpServletRequest hRequest = (HttpServletRequest) request;
-    HttpServletResponse hResponse = (HttpServletResponse) response;
+   /**
+    * 
+    * @param request ServletRequest
+    * @param response ServletResponse
+    * @param chain FilterChain
+    * @throws IOException
+    * @throws ServletException
+    */
+   public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException
+   {
+      HttpServletRequest hRequest = (HttpServletRequest) request;
+      HttpServletResponse hResponse = (HttpServletResponse) response;
 
-    Context sessionContext = new WebSessionContext(
-        ContextAdaptor.getSession(hRequest.getSession()));
+      Context sessionContext = new WebSessionContext(ContextAdaptor.getSession(hRequest
+               .getSession()));
 
-    Identity ident = (Identity)sessionContext.get(Seam.getComponentName(Identity.class));
+      Identity ident = (Identity) sessionContext.get(Seam.getComponentName(Identity.class));
 
-    if (!checkSecurityConstraints(hRequest.getServletPath(), hRequest.getMethod(), ident))
-      hResponse.sendRedirect(String.format("%s%s", hRequest.getContextPath(),
-                                           config.getSecurityErrorPage()));
-
-    chain.doFilter(request, response);
-  }
-
-  /**
-   * Performs a security check for a specified uri and method, for the specified
-   * Identity
-   *
-   * @param uri String
-   * @param method String
-   * @param ident Identity
-   * @return boolean
-   */
-  protected boolean checkSecurityConstraints(String uri, String method, Identity ident)
-  {
-    for (SecurityConstraint c : config.getSecurityConstraints())
-    {
-      if (c.included(uri, method))
+      if (!checkSecurityConstraints(hRequest.getServletPath(), hRequest.getMethod(), ident))
       {
-        if (ident == null || !userHasRole(ident, c.getAuthConstraint().getRoles()))
-          return false;
+         hResponse.sendRedirect(String.format("%s%s", hRequest.getContextPath(), config
+                  .getSecurityErrorPage()));
       }
-    }
+      else
+      {
+         chain.doFilter(request, response);
+      }
+   }
 
-    return true;
-  }
+   /**
+    * Performs a security check for a specified uri and method, for the
+    * specified Identity
+    * 
+    * @param uri String
+    * @param method String
+    * @param ident Identity
+    * @return boolean
+    */
+   protected boolean checkSecurityConstraints(String uri, String method, Identity ident)
+   {
+      for (SecurityConstraint c : config.getSecurityConstraints())
+      {
+         if (c.included(uri, method))
+         {
+            if (ident == null || !userHasRole(ident, c.getAuthConstraint().getRoles())) return false;
+         }
+      }
 
-  /**
-   * Returns true if the specified Identity has any one of a number of specified roles.
-   *
-   * @param ident Identity
-   * @param roles String[]
-   * @return boolean
-   */
-  private boolean userHasRole(Identity ident, Set<String> roles)
-  {
-    for (String role : roles)
-    {
-      if (ident.isUserInRole(role))
-        return true;
-    }
+      return true;
+   }
 
-    return false;
-  }
+   /**
+    * Returns true if the specified Identity has any one of a number of
+    * specified roles.
+    * 
+    * @param ident Identity
+    * @param roles String[]
+    * @return boolean
+    */
+   private boolean userHasRole(Identity ident, Set<String> roles)
+   {
+      for (String role : roles)
+      {
+         if (ident.isUserInRole(role)) return true;
+      }
 
-  public void destroy() {}
+      return false;
+   }
+
+   public void destroy()
+   {
+   }
 }
