2e52aba9a27c0122be90acb4855a6f5fd4a9877e
Pete Muir <pmuir@bleepbleep.org.uk>
2007-01-24 22:43:11 +0000

    Update docs with example, keep examples/mail in sync, make example use API send normally

---
+++ examples/mail/src/org/jboss/seam/example/mail/MailExample.java
@@ -0,0 +1,49 @@
+package org.jboss.seam.example.mail;
+
+import javax.mail.MessagingException;
+
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.core.FacesMessages;
+import org.jboss.seam.core.Renderer;
+
+@Name("mailExample")
+public class MailExample
+{
+   
+
+   
+   @In(create=true)
+   private FacesMessages facesMessages;
+   
+   @In(create=true)
+   private Renderer renderer;
+   
+   public void send() {
+      try {
+        renderer.render("/simple.xhtml");
+        facesMessages.add("Email sent successfully");
+      } catch (Exception e) {
+         facesMessages.add("Email sending failed: " + e.getMessage());
+      }
+   }
+   
+   public void sendHtml() {
+      try {
+         renderer.render("/html.xhtml");
+         facesMessages.add("Email sent successfully");
+      } catch (Exception e) {
+         facesMessages.add("Email sending failed");
+      }
+    }
+
+   public void sendPlain() {
+      try {
+         renderer.render("/plain.xhtml");
+         facesMessages.add("Email sent successfully");
+      } catch (Exception e) {
+         facesMessages.add("Email sending failed");
+      }
+    }
+   
+}
