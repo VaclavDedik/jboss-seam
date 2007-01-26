b7bde89304776dc0e5797dc9c2f512fb944a3642
Shane Bryzak <sbryzak@redhat.com>
2007-01-26 07:15:32 +0000

    fixed NPE

--- src/ui/org/jboss/seam/ui/UIFormattedText.java
+++ src/ui/org/jboss/seam/ui/UIFormattedText.java
@@ -1,32 +1,24 @@
 package org.jboss.seam.ui;
-
 import java.io.IOException;
 import java.io.Reader;
 import java.io.StringReader;
-
 import javax.faces.component.UIOutput;
 import javax.faces.context.FacesContext;
-
 import org.jboss.seam.text.L;
 import org.jboss.seam.text.P;
-
 import antlr.ANTLRException;
-
 public class UIFormattedText extends UIOutput             
 {
    public static final String COMPONENT_FAMILY = "org.jboss.seam.ui.FormattedText";
-
    @Override
    public String getFamily()
    {
       return COMPONENT_FAMILY;
    }
-
    @Override
    public void encodeBegin(FacesContext context) throws IOException
    {
-      if ( !isRendered() ) return;
-
+      if ( !isRendered() || getValue() == null) return;
       Reader r = new StringReader( (String) getValue() );
       L lexer = new L(r);
       P parser = new P(lexer);
@@ -38,7 +30,6 @@ public class UIFormattedText extends UIOutput
       {
          throw new RuntimeException(re);
       }
-
       context.getResponseWriter().write(parser.toString());
    }
    
