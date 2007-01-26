4321f05e28c3c32d1a483e1dcc89532388640f98
Shane Bryzak <sbryzak@redhat.com>
2007-01-26 14:20:34 +0000

    added no_image

--- examples/seamspace/src/org/jboss/seam/example/seamspace/ContentServlet.java
+++ examples/seamspace/src/org/jboss/seam/example/seamspace/ContentServlet.java
@@ -3,7 +3,9 @@ package org.jboss.seam.example.seamspace;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 import javax.imageio.ImageIO;
 import javax.servlet.ServletException;
@@ -29,6 +31,31 @@ public class ContentServlet extends HttpServlet
     * The maximum width allowed for image rescaling
     */
    private static final int MAX_IMAGE_WIDTH = 1024;
+   
+   private byte[] noImage;
+   
+   public ContentServlet()
+   {
+      InputStream in = getClass().getResourceAsStream("/images/no_image.png");
+      if (in != null)
+      {
+         ByteArrayOutputStream out = new ByteArrayOutputStream();
+         byte[] buffer = new byte[512];
+         try
+         {
+            int read = in.read(buffer);
+            while (read != -1)
+            {
+               out.write(buffer, 0, read);
+               read = in.read(buffer);
+            }
+            
+            noImage = out.toByteArray();
+         } 
+         catch (IOException e) { }
+      }
+      
+   }
 
    @Override
    protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -39,32 +66,46 @@ public class ContentServlet extends HttpServlet
          ContentLocal contentAction = (ContentLocal) Component.getInstance(ContentAction.class);
 
          MemberImage mi = contentAction.getImage(Integer.parseInt(request.getParameter("id")));
-
-         if (mi != null)
+         
+         String contentType = null;
+         byte[] data = null;
+         
+         if (mi != null && mi.getData() != null && mi.getData().length > 0)
          {
-            response.setContentType(mi.getContentType());
-
+            contentType = mi.getContentType();
+            data = mi.getData();
+         }
+         else if (noImage != null)
+         {
+            contentType = "image/png";
+            data = noImage;
+         }
+         
+         if (data != null)
+         {
+            response.setContentType(contentType);
+   
             boolean rescale = false;
             int width = 0;
             ImageIcon icon = null;
-
+   
             // Check if the image needs to be rescaled
             if (request.getParameter("width") != null)
             {
                width = Math.min(MAX_IMAGE_WIDTH, Integer.parseInt(request
                      .getParameter("width")));
-               icon = new ImageIcon(mi.getData());
+               icon = new ImageIcon(data);
                if (width > 0 && width != icon.getIconWidth())
                   rescale = true;
             }
-
+   
             // Rescale the image if required
             if (rescale)
             {
                double ratio = (double) width / icon.getIconWidth();
                int height = (int) (icon.getIconHeight() * ratio);
                
-               int imageType = "image/png".equals(mi.getContentType()) ? 
+               int imageType = "image/png".equals(contentType) ? 
                      BufferedImage.TYPE_INT_ARGB : BufferedImage.TYPE_INT_RGB;                  
                BufferedImage bImg = new BufferedImage(width, height, imageType);
                Graphics2D g2d = bImg.createGraphics();
@@ -72,20 +113,22 @@ public class ContentServlet extends HttpServlet
                      RenderingHints.VALUE_INTERPOLATION_BICUBIC);
                g2d.drawImage(icon.getImage(), 0, 0, width, height, null);
                g2d.dispose();
-
+   
                String formatName = "";
-               if ("image/png".equals(mi.getContentType()))
+               if ("image/png".equals(contentType))
                   formatName = "png";
-               else if ("image/jpeg".equals(mi.getContentType()))
+               else if ("image/jpeg".equals(contentType))
                   formatName = "jpeg";
-
+   
                ImageIO.write(bImg, formatName, response.getOutputStream());
             }
             else
-               response.getOutputStream().write(mi.getData());
-
-            response.getOutputStream().flush();
+            {
+               response.getOutputStream().write(data);
+            }
          }
+
+         response.getOutputStream().flush();
       }
    }
 }
