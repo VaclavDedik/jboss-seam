4daa7eb10616e9e53e82902aef04c9ce9a235b21
Pete Muir <pmuir@bleepbleep.org.uk>
2007-02-02 16:49:10 +0000

    JBSEAM-694 Bundle meldware (SMTP component) from buni.org with mail example

--- examples/mail/src/org/jboss/seam/example/mail/MailExample.java
+++ examples/mail/src/org/jboss/seam/example/mail/MailExample.java
@@ -1,17 +1,22 @@
 package org.jboss.seam.example.mail;
 
+import javax.faces.application.FacesMessage;
+import javax.faces.application.FacesMessage.Severity;
 import javax.mail.MessagingException;
 
 import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Logger;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.core.FacesMessages;
 import org.jboss.seam.core.Renderer;
+import org.jboss.seam.log.Log;
 
 @Name("mailExample")
 public class MailExample
 {
    
-
+   @Logger
+   private Log log;
    
    @In(create=true)
    private FacesMessages facesMessages;
@@ -24,7 +29,8 @@ public class MailExample
         renderer.render("/simple.xhtml");
         facesMessages.add("Email sent successfully");
       } catch (Exception e) {
-         facesMessages.add("Email sending failed: " + e.getMessage());
+         log.debug("Error send mail", e);
+         facesMessages.add(FacesMessage.SEVERITY_INFO, "Email sending failed: " + e.getMessage());
       }
    }
    
