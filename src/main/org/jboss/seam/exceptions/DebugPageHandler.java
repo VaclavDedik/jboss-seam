15853c42b0d6cd144ff629bc037813eb60dfa7a4
Gavin King <gavin.king@gmail.com>
2007-02-04 02:38:29 +0000

    bring back debug page

--- src/main/org/jboss/seam/exceptions/DebugPageHandler.java
+++ src/main/org/jboss/seam/exceptions/DebugPageHandler.java
@@ -3,9 +3,7 @@
  */
 package org.jboss.seam.exceptions;
 
-import org.jboss.seam.contexts.Context;
 import org.jboss.seam.contexts.Contexts;
-import org.jboss.seam.contexts.Lifecycle;
 import org.jboss.seam.core.Exceptions;
 import org.jboss.seam.core.Manager;
 
@@ -16,17 +14,14 @@ public class DebugPageHandler extends ExceptionHandler
    public void handle(Exception e) throws Exception
    {
       Exceptions.log.error("redirecting to debug page", e);
-      Context conversationContext = Contexts.getConversationContext();
-      conversationContext.set("org.jboss.seam.debug.lastException", e);
-      conversationContext.set("org.jboss.seam.debug.phaseId", Lifecycle.getPhaseId().toString());
+      Contexts.getConversationContext().set("org.jboss.seam.debug.lastException", e);
       org.jboss.seam.core.Redirect redirect = org.jboss.seam.core.Redirect.instance();
       redirect.setViewId("/debug.xhtml");
       Manager manager = Manager.instance();
       manager.beforeRedirect();
       redirect.setParameter( manager.getConversationIdParameter(), manager.getCurrentConversationId() );
       redirect.execute();
-      conversationContext.flush();
-      //(e);
+      Contexts.getConversationContext().flush();
    }
 
    @Override
