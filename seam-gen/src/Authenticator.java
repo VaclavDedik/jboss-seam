ea41057dc97e90ec34f463738827b3dffc1fed22
Gavin King <gavin.king@gmail.com>
2007-02-07 00:06:40 +0000

    update to latest security pkg

--- seam-gen/src/Authenticator.java
+++ seam-gen/src/Authenticator.java
@@ -1,23 +1,26 @@
 package @actionPackage@;
 
-import java.util.Set;
-
+import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Logger;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.log.Log;
+import org.jboss.seam.security.Identity;
 
 
 @Name("authenticator")
 public class Authenticator
 {
     @Logger Log log;
+    
+    @In Identity identity;
    
-    public boolean authenticate(String username, String password, Set<String> roles)
+    public boolean authenticate()
     {
-        log.info("authenticating #0", username);
+        log.info("authenticating #0", identity.getUsername());
         //write your authentication logic here,
         //return true if the authentication was
         //successful, false otherwise
+        identity.addRole("admin");
         return true;
     }
 }
