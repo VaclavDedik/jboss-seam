c77e5c802b0b514c759a795efa6be0a9fe4bd8ba
Shane Bryzak <sbryzak@redhat.com>
2007-02-08 15:45:47 +0000

    redundant annotations

--- src/main/org/jboss/seam/mock/MockSecureEntity.java
+++ src/main/org/jboss/seam/mock/MockSecureEntity.java
@@ -1,13 +1,12 @@
 package org.jboss.seam.mock;
 
 import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 import org.jboss.seam.annotations.Install;
 import org.jboss.seam.annotations.Name;
-import org.jboss.seam.annotations.security.Permission;
-import org.jboss.seam.annotations.security.Permissions;
 
 /**
  * Used by security unit tests
@@ -16,10 +15,6 @@ import org.jboss.seam.annotations.security.Permissions;
  */
 @Name("mockSecureEntity")
 @Install(false)
-@Permissions( {
-      @Permission(action = "read", expr = "#{aclPermissionChecker.checkPermission}"),
-      @Permission(action = "delete", expr = "#{aclPermissionChecker.checkPermission}"),
-      @Permission(action = "special", expr = "#{aclPermissionChecker.checkPermission}") })
 @Entity
 public class MockSecureEntity implements Serializable
 {
