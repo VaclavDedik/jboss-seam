3b3e9dec1f5fb2e167bf27253a01d3b7663d7a5c
Shane Bryzak <sbryzak@redhat.com>
2007-02-08 00:19:39 +0000

    Fixed my name, Australian geography :)

--- examples/contactlist/resources/import.sql
+++ examples/contactlist/resources/import.sql
@@ -1,3 +1,3 @@
 insert into Contact ( firstName, lastName, cellPhone, address, city, state, zip, country, version ) values ( 'Gavin', 'King', null, 'Peachtree Rd', 'Atlanta', 'GA', '30305', 'USA', 1 )
 insert into Contact ( firstName, lastName, cellPhone, address, city, state, zip, country, version ) values ( 'Norman', 'Richards', null, null, 'Austin', 'TX', '78701', 'USA', 1 )
-insert into Contact ( firstName, lastName, cellPhone, address, city, state, zip, country, version ) values ( 'Shane', 'Bryzack', null, null, 'Brisbane', 'Vic', null, 'Australia', 1 )
+insert into Contact ( firstName, lastName, cellPhone, address, city, state, zip, country, version ) values ( 'Shane', 'Bryzak', null, null, 'Brisbane', 'QLD', null, 'Australia', 1 )
