184364eaaf4fac0377e3e7b803dbfb38e93b0e21
Pete Muir <pmuir@bleepbleep.org.uk>
2007-02-09 20:15:13 +0000

    JBSEAM-805

--- src/mail/org/jboss/seam/mail/ui/UIBody.java
+++ src/mail/org/jboss/seam/mail/ui/UIBody.java
@@ -12,78 +12,93 @@ import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMultipart;
 
 /**
- * JSF component for rendering the body
- * Supports plain text, html bodies and setting an alternative
- * (text) part using an alternative facet
- *
+ * JSF component for rendering the body Supports plain text, html bodies and
+ * setting an alternative (text) part using an alternative facet
+ * 
  */
 public class UIBody extends MailComponent
 {
-   
-   public static final String HTML="html";
+
+   public static final String HTML = "html";
+
    public static final String PLAIN = "plain";
-   
+
    private String type = HTML;
-   
+
    @Override
    public void encodeChildren(FacesContext facesContext) throws IOException
    {
-     try
-     {
-        String body = encode(facesContext);
-        BodyPart bodyPart = new MimeBodyPart();
-        if (PLAIN.equalsIgnoreCase(type)) 
-        {
-          bodyPart.setText(body);
-          bodyPart.setDisposition("inline");
-        }
-        else if (HTML.equals(type)) 
-        {
-           UIComponent alternative = getFacet("alternative");
-           if (alternative != null)
-           {
-              BodyPart text = new MimeBodyPart();
-              text.setText(encode(facesContext,alternative));
-              text.setDisposition("inline");
-              
-              BodyPart html = new MimeBodyPart();
-              html.setContent(body, "text/html");
-              html.setDisposition("inline");
-              Multipart multipart = new MimeMultipart("alternative");
-              multipart.addBodyPart(text);
-              multipart.addBodyPart(html);
-              bodyPart.setContent(multipart);
-              
-           }
-           else
-           {   
-              bodyPart.setContent(body, "text/html");
-              bodyPart.setDisposition("inline");
-           }
-        }
-        getRootMultipart().addBodyPart(bodyPart);
+      try
+      {
+         String body = encode(facesContext);
+         BodyPart bodyPart = null;
+         if (PLAIN.equalsIgnoreCase(type))
+         {
+            bodyPart = getTextBody(facesContext, body);
+         }
+         else if (HTML.equals(type))
+         {
+            UIComponent alternative = getFacet("alternative");
+            if (alternative != null)
+            {
+               Multipart multipart = new MimeMultipart("alternative");
+
+               multipart.addBodyPart(getTextBody(facesContext, encode(facesContext,
+                        alternative)));
+               multipart.addBodyPart(getHtmlBody(facesContext, body));
+
+               bodyPart = new MimeBodyPart();
+               bodyPart.setContent(multipart);
+
+            }
+            else
+            {
+               bodyPart = getHtmlBody(facesContext, body);
+            }
+         }
+         getRootMultipart().addBodyPart(bodyPart);
       }
       catch (MessagingException e)
       {
          throw new FacesException(e.getMessage(), e);
       }
    }
-   
+
    public void setType(String type)
    {
       this.type = type;
    }
-   
+
    /**
     * The type of the body - plain or html
     */
    public String getType()
    {
-      if (type == null) 
+      if (type == null)
       {
          return getString("type");
       }
       return type;
+   }
+
+   private BodyPart getTextBody(FacesContext facesContext, Object body)
+            throws MessagingException
+   {
+      BodyPart bodyPart = new MimeBodyPart();
+      bodyPart.setDisposition("inline");
+      bodyPart.setContent(body, "text/plain; charset="
+               + facesContext.getResponseWriter().getCharacterEncoding() + "; format=flowed");
+      return bodyPart;
+   }
+
+   private BodyPart getHtmlBody(FacesContext facesContext, Object body)
+            throws MessagingException
+   {
+      BodyPart bodyPart = new MimeBodyPart();
+      bodyPart.setDisposition("inline");
+      bodyPart.setContent(body, "text/html; charset="
+               + facesContext.getResponseWriter().getCharacterEncoding());
+      return bodyPart;
    }
 
 }
