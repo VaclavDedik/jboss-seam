2f79f2ecd566ebcdd56a351592c1e79af1606a3f
Norman Richards <orb@jump.net>
2007-02-12 22:36:39 +0000

    clean up

--- examples/spring/src/org/jboss/seam/ioc/spring/SeamPostProcessor.java
+++ examples/spring/src/org/jboss/seam/ioc/spring/SeamPostProcessor.java
@@ -24,122 +24,119 @@ import org.springframework.util.ClassUtils;
  *
  * @author youngm
  */
-public class SeamPostProcessor implements BeanFactoryPostProcessor, InitializingBean
+public class SeamPostProcessor 
+    implements BeanFactoryPostProcessor, 
+               InitializingBean
 {
-   private static final LogProvider log = Logging.getLogProvider(SeamPostProcessor.class);
-
-   /**
-    * Default seam scope prefix.
-    */
-   public static final String DEFAULT_SCOPE_PREFIX = "seam.";
-
-   private String scopePrefix;
-
-    public SeamPostProcessor() {
-        System.out.println("****************** new seam post processor");
+    private static final LogProvider log = Logging.getLogProvider(SeamPostProcessor.class);
+    
+    /**
+     * Default seam scope prefix.
+     */
+    public static final String DEFAULT_SCOPE_PREFIX = "seam.";
+    
+    private String scopePrefix;
+    
+    
+    /**
+     * Null is not a valid scopePrefix so make it the default is used if null or empty.
+     *
+     * @see org.springframework.beans.factory.InitializingBean#afterPropertiesSet()
+     */
+    public void afterPropertiesSet() throws Exception
+    {
+        if (scopePrefix == null || "".equals(scopePrefix))
+        {
+            scopePrefix = DEFAULT_SCOPE_PREFIX;
+        }
     }
 
     /**
-    * Null is not a valid scopePrefix so make it the default is used if null or empty.
-    *
-    * @see org.springframework.beans.factory.InitializingBean#afterPropertiesSet()
-    */
-   public void afterPropertiesSet() throws Exception
-   {
-       System.out.println("****************** after properties set");
-      if (scopePrefix == null || "".equals(scopePrefix))
-      {
-         scopePrefix = DEFAULT_SCOPE_PREFIX;
-      }
-   }
-
-   /**
-    * Add all of the seam scopes to this beanFactory.
-    *
-    * @see org.springframework.beans.factory.config.BeanFactoryPostProcessor#postProcessBeanFactory(org.springframework.beans.factory.config.ConfigurableListableBeanFactory)
-    */
-   public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException
-   {
-       System.out.println("****************** post process");
-      for (ScopeType scope : ScopeType.values())
-      {
-         // Don't create a scope for UnSpecified
-         if (scope != ScopeType.UNSPECIFIED)
-         {
-            beanFactory.registerScope(scopePrefix + scope.name(), new SeamScope(scope));
-         }
-      }
-      // Create a mock application context if not available.
-      boolean unmockApplication = false;
-      if (!Contexts.isApplicationContextActive())
-      {
-         Lifecycle.mockApplication();
-         unmockApplication = true;
-      }
-      try
-      {
-         Context applicationContext = Contexts.getApplicationContext();
-         // Iterate through all the beans in the factory
-         for (String beanName : beanFactory.getBeanDefinitionNames())
-         {
-            BeanDefinition definition = beanFactory.getBeanDefinition(beanName);
-            Class beanClass;
-            ScopeType scope;
-            if (definition.getScope().startsWith(scopePrefix))
+     * Add all of the seam scopes to this beanFactory.
+     *
+     * @see org.springframework.beans.factory.config.BeanFactoryPostProcessor#postProcessBeanFactory(org.springframework.beans.factory.config.ConfigurableListableBeanFactory)
+     */
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException
+    {
+        for (ScopeType scope : ScopeType.values())
+        {
+            // Don't create a scope for UnSpecified
+            if (scope != ScopeType.UNSPECIFIED)
             {
-               scope = ScopeType.valueOf(definition.getScope().replaceFirst(scopePrefix, "").toUpperCase());
+                beanFactory.registerScope(scopePrefix + scope.name(), new SeamScope(scope));
             }
-            else
+        }
+        // Create a mock application context if not available.
+        boolean unmockApplication = false;
+        if (!Contexts.isApplicationContextActive())
             {
-               if (log.isDebugEnabled())
-               {
-                  log.debug("No scope could be derived for bean with name: " + beanName);
-               }
-               continue;
+                Lifecycle.mockApplication();
+                unmockApplication = true;
             }
-            if (scope == ScopeType.UNSPECIFIED)
+        try
+        {
+            Context applicationContext = Contexts.getApplicationContext();
+            // Iterate through all the beans in the factory
+            for (String beanName : beanFactory.getBeanDefinitionNames())
             {
-               if (log.isDebugEnabled())
-               {
-                  log.debug("Discarding bean with scope UNSPECIFIED.  Spring will throw an appropriate error later: " + beanName);
-               }
-               continue;
+                BeanDefinition definition = beanFactory.getBeanDefinition(beanName);
+                Class beanClass;
+                ScopeType scope;
+                if (definition.getScope().startsWith(scopePrefix))
+                {
+                    scope = ScopeType.valueOf(definition.getScope().replaceFirst(scopePrefix, "").toUpperCase());
+                }
+                else
+                {
+                    if (log.isDebugEnabled())
+                    {
+                        log.debug("No scope could be derived for bean with name: " + beanName);
+                    }
+                    continue;
+                }
+                if (scope == ScopeType.UNSPECIFIED)
+                {
+                    if (log.isDebugEnabled())
+                    {
+                        log.debug("Discarding bean with scope UNSPECIFIED.  Spring will throw an appropriate error later: " + beanName);
+                    }
+                    continue;
+                }
+                // Cannot be a seam component without a class maybe later
+                if (definition.getBeanClassName() == null)
+                {
+                    throw new FatalBeanException("Seam scoped bean must explicitly define a class.");
+                }
+                else
+                {
+                    try
+                    {
+                        beanClass = ClassUtils.forName(definition.getBeanClassName());
+                    }
+                    catch (ClassNotFoundException e)
+                    {
+                        throw new FatalBeanException("Error", e);
+                    }
+                }
+                // Add the component to seam
+                applicationContext.set(beanName + Initialization.COMPONENT_SUFFIX, new SpringComponent(beanClass, beanName, scope, beanFactory));
             }
-            // Cannot be a seam component without a class maybe later
-            if (definition.getBeanClassName() == null)
+        }
+        finally
+        {
+            if (unmockApplication)
             {
-               throw new FatalBeanException("Seam scoped bean must explicitly define a class.");
+                Lifecycle.unmockApplication();
             }
-            else
-            {
-               try
-               {
-                  beanClass = ClassUtils.forName(definition.getBeanClassName());
-               }
-               catch (ClassNotFoundException e)
-               {
-                  throw new FatalBeanException("Error", e);
-               }
-            }
-            // Add the component to seam
-            applicationContext.set(beanName + Initialization.COMPONENT_SUFFIX, new SpringComponent(beanClass, beanName, scope, beanFactory));
-         }
-      }
-      finally
-      {
-         if (unmockApplication)
-         {
-            Lifecycle.unmockApplication();
-         }
-      }
-   }
-
-   /**
-    * @param scopePrefix the prefix to use to identify seam scopes for spring
-    *                    beans
-    */
-   public void setScopePrefix(String scopePrefix)
-   {
-      this.scopePrefix = scopePrefix;
-   }
+        }
+    }
+    
+    /**
+     * @param scopePrefix the prefix to use to identify seam scopes for spring
+     *                    beans
+     */
+    public void setScopePrefix(String scopePrefix)
+    {
+        this.scopePrefix = scopePrefix;
+    }
 }
