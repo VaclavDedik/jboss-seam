8a7af3bc4722ab3bd83abeb82d99adfb6f079a55
Gavin King <gavin.king@gmail.com>
2007-02-13 06:17:38 +0000

    JBSEAM-836

--- src/main/org/jboss/seam/interceptors/SecurityInterceptor.java
+++ src/main/org/jboss/seam/interceptors/SecurityInterceptor.java
@@ -2,12 +2,12 @@ package org.jboss.seam.interceptors;
 
 import java.lang.reflect.Method;
 
-import org.jboss.seam.Seam;
 import org.jboss.seam.annotations.AroundInvoke;
 import org.jboss.seam.annotations.Interceptor;
 import org.jboss.seam.annotations.security.Restrict;
 import org.jboss.seam.intercept.InvocationContext;
 import org.jboss.seam.security.Identity;
+import org.jboss.seam.util.Strings;
 
 /**
  * Provides authorization services for component invocations.
@@ -23,28 +23,31 @@ public class SecurityInterceptor extends AbstractInterceptor
    @AroundInvoke
    public Object aroundInvoke(InvocationContext invocation) throws Exception
    {
-      Restrict r = null;
-
       Method method = invocation.getMethod();
-
-      if (method.isAnnotationPresent(Restrict.class))
+      Restrict restrict = getRestriction(method);
+      if (restrict != null)
       {
-         r = method.getAnnotation(Restrict.class);
-      }
-      else if (method.getDeclaringClass().isAnnotationPresent(Restrict.class))
-      {
-         r = method.getDeclaringClass().getAnnotation(Restrict.class);
-      }
-
-      if (r != null)
-      {
-         String expr = r.value() != null && !"".equals(r.value()) ? r.value() : 
-            createDefaultExpr(method);         
-         
+         String expr = !Strings.isEmpty( restrict.value() ) ? 
+                  restrict.value() : createDefaultExpr(method);
          Identity.instance().checkRestriction(expr);
       }
-
       return invocation.proceed();
+   }
+
+   private Restrict getRestriction(Method method)
+   {
+      if ( method.isAnnotationPresent(Restrict.class) )
+      {
+         return method.getAnnotation(Restrict.class);
+      }
+      else if ( method.getDeclaringClass().isAnnotationPresent(Restrict.class) )
+      {
+         if ( !getComponent().isLifecycleMethod(method) )
+         {
+            return method.getDeclaringClass().getAnnotation(Restrict.class);
+         }
+      }
+      return null;
    }
    
    /**
@@ -56,13 +59,6 @@ public class SecurityInterceptor extends AbstractInterceptor
     */
    private String createDefaultExpr(Method method)
    {
-      String name = Seam.getComponentName(method.getDeclaringClass());
-      if (name == null)
-      {
-         throw new IllegalArgumentException(String.format(
-                  "Method %s is not a component method", method));
-      }
-      
-      return String.format("#{s:hasPermission('%s','%s', null)}", name, method.getName());
+      return String.format( "#{s:hasPermission('%s','%s', null)}", getComponent().getName(), method.getName() );
    }
 }
