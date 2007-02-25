0977e423c7e26dd390781062064e349954963f7f
Gavin King <gavin.king@gmail.com>
2007-02-25 22:34:54 +0000

    oops, forgot this

--- src/gen/org/jboss/seam/tool/Util.java
+++ src/gen/org/jboss/seam/tool/Util.java
@@ -1,5 +1,8 @@
 package org.jboss.seam.tool;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Util
 {
    public String lower(String name)
@@ -9,5 +12,9 @@ public class Util
    public String upper(String name)
    {
       return name.substring(0, 1).toUpperCase() + name.substring(1);
+   }
+   public Set set()
+   {
+      return new HashSet();
    }
 }
