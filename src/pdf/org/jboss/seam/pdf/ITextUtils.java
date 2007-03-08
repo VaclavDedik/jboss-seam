106045b2c0f79b803766e5158d658f2dd949832c
Norman Richards <orb@jump.net>
2007-03-08 02:55:49 +0000

    use itext2 convenience functions

--- src/pdf/org/jboss/seam/pdf/ITextUtils.java
+++ src/pdf/org/jboss/seam/pdf/ITextUtils.java
@@ -34,52 +34,11 @@ public class ITextUtils {
      *  so we'll copy that logic here. 
      */
     public static int alignmentValue(String alignment) {
-        if (ElementTags.ALIGN_CENTER.equalsIgnoreCase(alignment)) {
-            return Element.ALIGN_CENTER;
-        }
-        if (ElementTags.ALIGN_LEFT.equalsIgnoreCase(alignment)) {
-            return Element.ALIGN_LEFT;
-        }
-        if (ElementTags.ALIGN_RIGHT.equalsIgnoreCase(alignment)) {
-            return Element.ALIGN_RIGHT;
-        }
-        if (ElementTags.ALIGN_JUSTIFIED.equalsIgnoreCase(alignment)) {
-            return Element.ALIGN_JUSTIFIED;
-        }
-        if (ElementTags.ALIGN_JUSTIFIED_ALL.equalsIgnoreCase(alignment)) {
-            return Element.ALIGN_JUSTIFIED_ALL;
-        }
-        if (ElementTags.ALIGN_TOP.equalsIgnoreCase(alignment)) {
-            return Element.ALIGN_TOP;
-        }
-        if (ElementTags.ALIGN_MIDDLE.equalsIgnoreCase(alignment)) {
-            return Element.ALIGN_MIDDLE;
-        }
-        if (ElementTags.ALIGN_BOTTOM.equalsIgnoreCase(alignment)) {
-            return Element.ALIGN_BOTTOM;
-        }
-        if (ElementTags.ALIGN_BASELINE.equalsIgnoreCase(alignment)) {
-            return Element.ALIGN_BASELINE;
-        }
-
-        return Element.ALIGN_UNDEFINED;
+        return ElementTags.alignmentValue(alignment);     
     }
     
     public static Rectangle pageSizeValue(String name)  {
-        if (name.indexOf(' ') != -1) {            
-            int[] sizes = stringToIntArray(name);
-            if (sizes.length != 2) {
-                throw new RuntimeException("page size should contain two integer numbers");
-            }
-            return new Rectangle(sizes[0],sizes[1]);
-        } 
-        
-        try {            
-            Field field = PageSize.class.getDeclaredField(name.toUpperCase());
-            return (Rectangle) field.get(null);
-        } catch (Exception e) {
-            throw new RuntimeException("Can't find page size " + name);          
-        }
+        return PageSize.getRectangle(name);
     }
     
     
