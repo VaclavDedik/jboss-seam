3a5c9a2a431a4cc4f199d407e792e2890b0f59d0
Gavin King <gavin.king@gmail.com>
2007-03-16 05:43:35 +0000

    minor

--- src/ui/org/jboss/seam/ui/UIFormattedText.java
+++ src/ui/org/jboss/seam/ui/UIFormattedText.java
@@ -12,21 +12,15 @@ import org.jboss.seam.text.SeamTextParser;
 import antlr.ANTLRException;
 public class UIFormattedText extends UIOutput             
 {
-   public static final String COMPONENT_TYPE   = "org.jboss.seam.ui.FormattedText";      
+   public static final String COMPONENT_TYPE = "org.jboss.seam.ui.FormattedText";      
    public static final String COMPONENT_FAMILY = "org.jboss.seam.ui.FormattedText";
-   
-   public static final String DEFAULT_RENDERER_TYPE = null;
-   
-   public UIFormattedText()
-   {
-      setRendererType(DEFAULT_RENDERER_TYPE);
-   }
    
    @Override
    public String getFamily()
    {
       return COMPONENT_FAMILY;
    }
+   
    @Override
    public void encodeBegin(FacesContext context) throws IOException
    {
