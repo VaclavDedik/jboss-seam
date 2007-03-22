dc2e737dda9ec6246a5c160a0be6d5ca4bbd2bce
Pete Muir <pmuir@bleepbleep.org.uk>
2007-03-22 15:20:07 +0000

    Tidy up and JBSEAM-1093

--- src/ui/org/jboss/seam/ui/facelet/FaceletsRenderer.java
+++ src/ui/org/jboss/seam/ui/facelet/FaceletsRenderer.java
@@ -2,7 +2,6 @@ package org.jboss.seam.ui.facelet;
 
 import java.io.IOException;
 import java.io.StringWriter;
-import java.io.Writer;
 import java.net.URL;
 
 import javax.faces.component.UIViewRoot;
@@ -29,92 +28,130 @@ import com.sun.facelets.impl.DefaultResourceResolver;
 @Scope(ScopeType.STATELESS)
 @Intercept(InterceptionType.NEVER)
 @Name("org.jboss.seam.core.renderer")
-@Install(value=true,precedence=Install.BUILT_IN,
-        classDependencies={"com.sun.facelets.Facelet"})
+@Install(value = true, precedence = Install.BUILT_IN, classDependencies = { "com.sun.facelets.Facelet" })
 public class FaceletsRenderer extends Renderer
 {
+   
+   private class Context 
+   {
+      
+      public Context(String viewId)
+      {
+         this.viewId = viewId;
+      }
+      
+      private String viewId;
+      
+      private ClassLoader originalClassLoader;
+      private ResponseWriter originalResponseWriter;
+      private UIViewRoot originalViewRoot;
+      private StringWriter writer = new StringWriter();
+      
+      public Context wrap() 
+      {
+         // Make sure we are using the correct ClassLoader
+         originalClassLoader = Thread.currentThread().getContextClassLoader();
+         Thread.currentThread().setContextClassLoader(this.getClass().getClassLoader());
+         
+         // If a FacesContext isn't available, set one up
+         if (FacesContext.getCurrentInstance() == null)
+         {
+            MockFacesContext mockFacesContext = new MockFacesContext(new MockExternalContext())
+                     .setCurrent();
+            mockFacesContext.createViewRoot();
+         }
+         
+         FacesContext facesContext = FacesContext.getCurrentInstance();
+         
+         // Wrap the ResponseWriter
+         originalResponseWriter = facesContext.getResponseWriter();
+         facesContext.setResponseWriter(facesContext.getRenderKit().createResponseWriter(writer,
+                  null, null));
+         
+         // Create a new UIViewRoot
+         originalViewRoot = facesContext.getViewRoot();
+         UIViewRoot viewRoot = new UIViewRoot();
+         viewRoot.setRenderKitId(facesContext.getApplication().getViewHandler().calculateRenderKitId(facesContext));
+         viewRoot.setViewId(viewId);
+         viewRoot.setLocale(originalViewRoot.getLocale());
+         facesContext.setViewRoot(viewRoot);
+         return this;
+      }
+      
+      public void unwrap() 
+      {
+         FacesContext facesContext = FacesContext.getCurrentInstance();
+         if (originalResponseWriter != null)
+         {
+            facesContext.setResponseWriter(originalResponseWriter);
+         }
+         if (originalViewRoot != null)
+         {
+            facesContext.setViewRoot(originalViewRoot);
+         }
+         Thread.currentThread().setContextClassLoader(originalClassLoader);
+         
+      }
+      
+      public String getWrittenOutput() 
+      {
+         return writer.getBuffer().toString(); 
+      }
+      
+   }
+   
 
-    @Override
-    public String render(String viewId) 
-    {
-       
-       // JBSEAM-950
-       ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-       
-      Thread.currentThread().setContextClassLoader(this.getClass().getClassLoader());
-       
-       if (FacesContext.getCurrentInstance() == null) 
-       {
-             MockFacesContext mockFacesContext = new MockFacesContext(new MockExternalContext()).setCurrent();
-             mockFacesContext.createViewRoot();
-       }
-        FacesContext facesContext = FacesContext.getCurrentInstance();
-
-        ResponseWriter originalWriter = facesContext.getResponseWriter();
-        
-
-        
-        
-
-        try 
-        {
-            StringWriter stringWriter = new StringWriter();
-            wrapResponseWriter(facesContext, stringWriter);
-
-            renderFacelet(facesContext, faceletForURL(resourceURL(viewId)));
-
-            return stringWriter.getBuffer().toString();
-        } 
-        catch (IOException e) 
-        {
-            throw new RuntimeException(e);
-        } 
-        finally 
-        {
-            if (originalWriter != null) 
-            {
-                facesContext.setResponseWriter(originalWriter);
-            }
-            Thread.currentThread().setContextClassLoader(classLoader);
-        }
-    }
-
-
-    protected URL resourceURL(String viewId) 
-    {
-       
-        URL url = Resources.getResource(viewId);
-
-        if (url == null) 
-        {
-            throw new IllegalArgumentException("resource doesn't exist: " + viewId);
-        }
-
-        return url;
+   @Override
+   public String render(String viewId)
+   {
+      Context context = new Context(viewId);
+      try
+      {
+         context.wrap();
+         FacesContext facesContext = FacesContext.getCurrentInstance();
+         renderFacelet(facesContext, faceletForURL(resourceURL(viewId)));
+         return context.getWrittenOutput();
+      }
+      catch (IOException e)
+      {
+         throw new RuntimeException(e);
+      }
+      finally
+      {
+         context.unwrap();
+      }
    }
 
-    protected Facelet faceletForURL(URL url) 
-        throws IOException
-    {
-        return new DefaultFaceletFactory(new SAXCompiler(), 
-                                         new DefaultResourceResolver()).getFacelet(url);
-    }
+   protected URL resourceURL(String viewId)
+   {
 
+      URL url = Resources.getResource(viewId);
 
-    protected void wrapResponseWriter(FacesContext facesContext, Writer writer) 
-    {
-        ResponseWriter responseWriter = 
-            facesContext.getRenderKit().createResponseWriter(writer, null, null);
+      if (url == null)
+      {
+         throw new IllegalArgumentException("resource doesn't exist: " + viewId);
+      }
 
-        facesContext.setResponseWriter(responseWriter);
-    }
+      return url;
+   }
 
-    protected void renderFacelet(FacesContext facesContext, Facelet facelet) 
-        throws IOException
-    {
-        UIViewRoot root = facesContext.getViewRoot();
-        facelet.apply(facesContext, root);
-        JSF.renderChildren(facesContext, root);
-    }
+   protected Facelet faceletForURL(URL url) throws IOException
+   {
+      return new DefaultFaceletFactory(new SAXCompiler(), new DefaultResourceResolver())
+               .getFacelet(url);
+   }
 
+   
+
+   protected void renderFacelet(FacesContext facesContext, Facelet facelet) throws IOException
+   {
+      UIViewRoot root = facesContext.getViewRoot();
+      facelet.apply(facesContext, root);
+      JSF.renderChildren(facesContext, root);
+   }
+   
+   
+
+   
+   
 }
