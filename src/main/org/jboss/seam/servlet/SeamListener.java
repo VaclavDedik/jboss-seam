1bb3d17504f576741ce20c394babad9d3a4b66de
Shane Bryzak <sbryzak@redhat.com>
2007-03-28 11:26:25 +0000

    JBSEAM-1120

--- src/main/org/jboss/seam/servlet/SeamListener.java
+++ src/main/org/jboss/seam/servlet/SeamListener.java
@@ -28,7 +28,7 @@ public class SeamListener implements ServletContextListener, HttpSessionListener
    private static final LogProvider log = Logging.getLogProvider(ServletContextListener.class);
 
    public void contextInitialized(ServletContextEvent event) {
-      log.info("Welcome to Seam 1.2.0.PATCH1");
+      log.info("Welcome to Seam 1.2.1");
       Lifecycle.setServletContext( event.getServletContext() );
       new Initialization( event.getServletContext() ).create().init();
    }
