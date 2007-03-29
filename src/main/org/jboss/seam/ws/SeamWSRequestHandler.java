46d6f35a456246aa5f42f47383cd3983ce0ecbff
Shane Bryzak <sbryzak@redhat.com>
2007-03-29 13:54:04 +0000

    ws request handler

---
+++ src/main/org/jboss/seam/ws/SeamWSRequestHandler.java
@@ -0,0 +1,50 @@
+package org.jboss.seam.ws;
+
+import java.util.Set;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.xml.ws.handler.MessageContext;
+import javax.xml.ws.handler.soap.SOAPHandler;
+
+import org.jboss.seam.contexts.Lifecycle;
+
+/**
+ * A SOAP request handler that instantiates the Seam contexts for a web service
+ * request.
+ *  
+ * @author Shane Bryzak
+ */
+public class SeamWSRequestHandler implements SOAPHandler
+{
+   public Set getHeaders()
+   {
+      return null;
+   }
+
+   public void close(MessageContext messageContext)
+   {
+ //     System.out.println("SeamWSRequestHandler.close()");
+      
+      Lifecycle.endRequest();
+   }
+
+   public boolean handleFault(MessageContext messageContext)
+   {
+//      System.out.println("SeamWSRequestHandler.handleFault()");
+
+      Lifecycle.endRequest();
+      
+      return true;
+   }
+
+   public boolean handleMessage(MessageContext messageContext)
+   {  
+      HttpServletRequest request = (HttpServletRequest) messageContext.get(MessageContext.SERVLET_REQUEST);      
+      Lifecycle.beginRequest(Lifecycle.getServletContext(), request.getSession(), request);
+      Lifecycle.resumeConversation(request.getSession());
+      
+//      System.out.println("SeamWSRequestHandler.handleMessage()");
+      return true;
+   }
+
+}
