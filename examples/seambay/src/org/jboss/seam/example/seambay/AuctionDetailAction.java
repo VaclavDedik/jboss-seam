394a499f86eb50e3800f055efdd0789952312478
Shane Bryzak <sbryzak@redhat.com>
2007-04-17 14:57:38 +0000

    linked search results to detail view

---
+++ examples/seambay/src/org/jboss/seam/example/seambay/AuctionDetailAction.java
@@ -0,0 +1,34 @@
+package org.jboss.seam.example.seambay;
+
+import javax.persistence.EntityManager;
+
+import org.jboss.seam.annotations.Factory;
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Name;
+
+@Name("auctionDetail")
+public class AuctionDetailAction
+{
+   @In EntityManager entityManager;
+   
+   private int selectedAuctionId;
+   
+   private Auction auction;
+   
+   @Factory("auction")
+   public Auction getAuction()
+   {
+      auction = entityManager.find(Auction.class, selectedAuctionId);      
+      return auction;
+   }
+   
+   public int getSelectedAuctionId()
+   {
+      return selectedAuctionId;
+   }
+   
+   public void setSelectedAuctionId(int selectedAuctionId)
+   {
+      this.selectedAuctionId = selectedAuctionId;
+   }
+}
