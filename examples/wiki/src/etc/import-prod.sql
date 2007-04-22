570c24178a587877054a106302204ab49cc31bb0
Christian Bauer <christian@hibernate.org>
2007-04-22 13:53:52 +0000

    Distribution target now generates valid SQL files ready for import in MySQL

--- examples/wiki/src/etc/import-prod.sql
+++ examples/wiki/src/etc/import-prod.sql
@@ -1,4 +1,7 @@
-// Minimum Wiki data - can be customized a little (names and such)
+-- MySQL magic!
+set session sql_mode='PIPES_AS_CONCAT'
+
+-- Minimum Wiki data - can be customized a little (names and such)
 insert into USERS (USER_ID, OBJ_VERSION, FIRSTNAME, LASTNAME, USERNAME, PASSWORDHASH, EMAIL, ACTIVATED, CREATED_ON) values (1, 0, 'System', 'Administrator', 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin@email.tld', true, '2006-06-27 13:45:00')
 insert into USERS (USER_ID, OBJ_VERSION, FIRSTNAME, LASTNAME, USERNAME, PASSWORDHASH, EMAIL, ACTIVATED, CREATED_ON) values (2, 0, 'Anonymous', 'Guest', 'guest', 'guest', 'guest', false, '1976-06-26 13:45:00')
 
@@ -13,12 +16,12 @@ insert into NODE (NODE_ID, NODE_TYPE, NODE_REVISION, CREATED_BY_USER_ID, AREA_NR
 insert into FEED (FEED_ID, OBJ_VERSION, TITLE, DESCRIPTION, AUTHOR, COPYRIGHT, PUBLISHED_ON, DIRECTORY_ID) values (1, 0, 'ROOT', null, 'Lacewiki', null, '1976-06-26 11:11:11', 1)
 insert into NODE (NODE_ID, NODE_TYPE, NODE_REVISION, CREATED_BY_USER_ID, AREA_NR, PARENT_NODE_ID, NODE_POSITION, DEFAULT_DOCUMENT_ID, OBJ_VERSION, NAME, WIKINAME, MENU_ITEM, CREATED_ON, LAST_MODIFIED_ON, WRITE_ACCESS_LEVEL, READ_ACCESS_LEVEL) values (2, 'DIRECTORY', 1, 1, 2, 1, 0, null, 0, 'Member Area', 'MemberArea', true, '2005-06-27 13:45:00', '2005-06-27 13:45:00', 1000, 0)
 
-// Link protocols, can be customized
+-- Link protocols, can be customized
 
 insert into LINK_PROTOCOL (LINK_PROTOCOL_ID, OBJ_VERSION, PREFIX, LINK) values (1, 0, 'jbseam', 'http://jira.jboss.com/jira/browse/JBSEAM-[[link]]')
 insert into LINK_PROTOCOL (LINK_PROTOCOL_ID, OBJ_VERSION, PREFIX, LINK) values (2, 0, 'hhh', 'http://opensource.atlassian.com/projects/hibernate/browse/HHH-[[link]]')
 
-// Required system default configuration
+-- Required system default configuration
 
 insert into PREFERENCE(PREF_ID, OBJ_VERSION, COMPONENT_NAME, PROPERTY_NAME, STRING_VALUE)   values (1, 0, 'wikiPreferences', 'baseUrl', 'http://www.lacewiki.org')
 insert into PREFERENCE(PREF_ID, OBJ_VERSION, COMPONENT_NAME, PROPERTY_NAME, STRING_VALUE)   values (2, 0, 'wikiPreferences', 'themeName', 'default')
@@ -58,7 +61,10 @@ insert into PREFERENCE(PREF_ID, OBJ_VERSION, COMPONENT_NAME, PROPERTY_NAME, STRI
 insert into PREFERENCE(PREF_ID, OBJ_VERSION, COMPONENT_NAME, PROPERTY_NAME, LONG_VALUE)     values (29, 0, 'flashPreferences', 'objectWidth', 350)
 insert into PREFERENCE(PREF_ID, OBJ_VERSION, COMPONENT_NAME, PROPERTY_NAME, LONG_VALUE)     values (30, 0, 'flashPreferences', 'objectHeight', 425)
 
-// Start document
+-- Start document
 insert into NODE (NODE_ID, NODE_TYPE, NODE_REVISION, CREATED_BY_USER_ID, AREA_NR, PARENT_NODE_ID, NODE_POSITION, OBJ_VERSION, NAME, WIKINAME, MENU_ITEM, CREATED_ON, LAST_MODIFIED_ON, WRITE_ACCESS_LEVEL, READ_ACCESS_LEVEL) values (3, 'DIRECTORY', 1, 1, 3, 1, 1, 0, 'LaceWiki Home', 'LaceWikiHome', true, '1976-06-16 13:45:00', '1976-06-16 13:45:00', 0, 0)
-insert into NODE (NODE_ID, NODE_TYPE, NODE_REVISION, CREATED_BY_USER_ID, AREA_NR, PARENT_NODE_ID, NODE_POSITION, OBJ_VERSION, NAME, WIKINAME, MENU_ITEM, CONTENT, CREATED_ON, LAST_MODIFIED_ON, WRITE_ACCESS_LEVEL, READ_ACCESS_LEVEL, NAME_AS_TITLE) values  (4, 'DOCUMENT', 1, 1, 3, 3, 0, 0, 'Welcome to LaceWiki!', 'WelcomeToLaceWiki', true, 'Login with admin-admin and change your password!', '2005-06-27 13:45:00', '2005-06-27 13:45:00', 0, 0, true)
+insert into NODE (NODE_ID, NODE_TYPE, NODE_REVISION, CREATED_BY_USER_ID, AREA_NR, PARENT_NODE_ID, NODE_POSITION, OBJ_VERSION, NAME, WIKINAME, MENU_ITEM, CONTENT, CREATED_ON, LAST_MODIFIED_ON, WRITE_ACCESS_LEVEL, READ_ACCESS_LEVEL, NAME_AS_TITLE) values  (4, 'DOCUMENT', 1, 1, 3, 3, 0, 0, 'Welcome to LaceWiki!', 'WelcomeToLaceWiki', true, 'You need to login as /admin/ with password /admin/ to change settings.@@LF@@@@LF@@Do not forget to change the admin password!', '2005-06-27 13:45:00', '2005-06-27 13:45:00', 0, 0, true)
 update NODE set DEFAULT_DOCUMENT_ID = 4 where NODE_ID = 3
+
+-- More MySQL magic!
+set session sql_mode=''
