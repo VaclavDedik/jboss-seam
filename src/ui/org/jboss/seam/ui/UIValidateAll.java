5252669dda5e4d6415ecc39dc0b1af9022b04809
Shane Bryzak <sbryzak@redhat.com>
2007-04-24 08:56:02 +0000

    JBSEAM-1236

--- src/ui/org/jboss/seam/ui/UIValidateAll.java
+++ src/ui/org/jboss/seam/ui/UIValidateAll.java
@@ -1,28 +1,43 @@
 package org.jboss.seam.ui;
 
+import java.io.IOException;
 import java.util.List;
 
 import javax.faces.component.EditableValueHolder;
 import javax.faces.component.UIComponent;
 import javax.faces.component.UIComponentBase;
+import javax.faces.context.FacesContext;
 
 public class UIValidateAll extends UIComponentBase
 {
    public static final String COMPONENT_FAMILY = "org.jboss.seam.ui.ValidateAll";
    public static final String COMPONENT_TYPE = "org.jboss.seam.ui.UIValidateAll";
-      
+   
+   private boolean validatorsAdded = false;
+   
    @Override
    public String getFamily()
    {
       return COMPONENT_FAMILY;
    }
+   
+   @Override
+   public boolean getRendersChildren()
+   {
+      return true;
+   }   
 
    @Override
-   public List getChildren()
+   public void encodeChildren(FacesContext context) 
+       throws IOException
    {
-      addValidators( super.getChildren() );
-      return super.getChildren();
-   }
+      if (!validatorsAdded)
+      {
+         addValidators( super.getChildren() );
+         validatorsAdded = true;
+      }
+      JSF.renderChildren(context, this);
+   }   
 
    private void addValidators(List children)
    {
@@ -31,7 +46,8 @@ public class UIValidateAll extends UIComponentBase
          if (child instanceof EditableValueHolder)
          {
             EditableValueHolder evh =  (EditableValueHolder) child;
-            if ( evh.getValidators().length==0 && evh.getValidator()==null )
+            
+            if ( evh.getValidators().length==0 && evh.getValidator()==null) 
             {
                evh.addValidator( new ModelValidator() );
             }
@@ -42,5 +58,29 @@ public class UIValidateAll extends UIComponentBase
          }
       }
    }
+   
+   private Object[] values;
 
+   @Override
+   public Object saveState(FacesContext context)
+   {
+      if (values == null)
+      {
+         values = new Object[2];
+      }
+
+      values[0] = super.saveState(context);
+      values[1] = validatorsAdded ? Boolean.TRUE : Boolean.FALSE;
+
+      return (values);
+   }
+
+   @Override
+   public void restoreState(FacesContext context, Object state)
+   {
+      values = (Object[]) state;
+      super.restoreState(context, values[0]);
+      
+      validatorsAdded = ((Boolean) values[1]).booleanValue();      
+   }      
 }
