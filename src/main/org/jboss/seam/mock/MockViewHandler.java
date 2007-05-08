d594ff5869c63233d8ef8dd7f15c34c8a12f173b
Gavin King <gavin.king@gmail.com>
2007-05-08 15:25:53 +0000

    error messages

--- src/main/org/jboss/seam/mock/MockViewHandler.java
+++ src/main/org/jboss/seam/mock/MockViewHandler.java
@@ -38,8 +38,11 @@ public class MockViewHandler extends ViewHandler {
       String servletPath = ctx.getExternalContext().getRequestServletPath();
       if ( Strings.isEmpty(pathInfo) ) 
       {
-         return contextPath + viewId.substring( 0, viewId.lastIndexOf('.') ) + 
-               servletPath.substring( servletPath.lastIndexOf('.') );
+         int loc = viewId.lastIndexOf('.');
+         if (loc<0) throw new IllegalArgumentException("no file extension in view id: " + viewId);
+         int sploc = servletPath.lastIndexOf('.');
+         if (sploc<0) throw new IllegalArgumentException("no file extension in servlet path: " + servletPath);
+         return contextPath + viewId.substring(0, loc) + servletPath.substring(sploc);
       }
       else
       {
