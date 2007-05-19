f65ea45a12b1bfa319f83f46c006147af4ca16e4
Gavin King <gavin.king@gmail.com>
2007-05-19 01:10:55 +0000

    JBSEAM-1334

--- src/main/org/jboss/seam/jbpm/SeamVariableResolver.java
+++ src/main/org/jboss/seam/jbpm/SeamVariableResolver.java
@@ -4,15 +4,28 @@ import org.jboss.seam.Component;
 import org.jboss.seam.core.Init;
 import org.jbpm.jpdl.el.ELException;
 import org.jbpm.jpdl.el.VariableResolver;
+import org.jbpm.jpdl.el.impl.JbpmVariableResolver;
 
-public class SeamVariableResolver implements VariableResolver {
+public class SeamVariableResolver implements VariableResolver 
+{
+   
+   private VariableResolver jbpmVariableResolver = new JbpmVariableResolver();
 
-	public Object resolveVariable(String name) throws ELException {
+	public Object resolveVariable(String name) throws ELException 
+   {
 	   name = name.replace('$', '.');
 	   Object instance = Component.getInstance(name, true);
       if (instance==null)
       {
-         return Init.instance().getRootNamespace().getChild(name);
+         instance = jbpmVariableResolver.resolveVariable(name);
+         if (instance==null)
+         {
+            return Init.instance().getRootNamespace().getChild(name);
+         }
+         else
+         {
+            return instance;
+         }
       }
       else
       {
