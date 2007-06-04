c1be6f3b900a4af1e6c18d1349a98e728113ccc0
Gavin King <gavin.king@gmail.com>
2007-06-04 17:08:46 +0000

    oops

--- src/main/org/jboss/seam/transaction/Transaction.java
+++ src/main/org/jboss/seam/transaction/Transaction.java
@@ -6,8 +6,10 @@ import javax.naming.NameNotFoundException;
 import javax.naming.NamingException;
 
 import org.jboss.seam.Component;
+import org.jboss.seam.InterceptionType;
 import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.Install;
+import org.jboss.seam.annotations.Intercept;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
 import org.jboss.seam.annotations.Unwrap;
@@ -26,6 +28,7 @@ import org.jboss.seam.util.Naming;
 @Name("org.jboss.seam.transaction.transaction")
 @Scope(ScopeType.STATELESS)
 @Install(precedence=BUILT_IN)
+@Intercept(InterceptionType.NEVER)
 public class Transaction
 {
 
