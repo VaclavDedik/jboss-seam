d5ee4c63b93a79349b2ba3e579a468860af55b4b
Gavin King <gavin.king@gmail.com>
2007-06-06 18:02:10 +0000

    roll back breakage

--- seam.bat
+++ seam.bat
@@ -17,7 +17,7 @@ if [%1] == [] (goto usage)
 
 if %SEAMTASK% == help (goto help)
 
-java -cp "%JAVA_HOME%\lib\tools.jar;%DIRNAME%\lib\ant-launcher.jar" -Dant.home="%DIRNAME%\lib" -Dant.library.dir="%DIRNAME%\lib" org.apache.tools.ant.launch.Launcher -buildfile "%SEAMGENDIR%\build.xml" %ARGS%  
+java -cp "%JAVA_HOME%\lib\tools.jar;%DIRNAME%\lib\ant.jar;%DIRNAME%\lib\ant-nodeps.jar;%DIRNAME%\lib\ant-launcher.jar" -Dant.home="%DIRNAME%\lib" org.apache.tools.ant.launch.Launcher -buildfile "%SEAMGENDIR%\build.xml" %ARGS%
 goto END_NO_PAUSE
 
 :usage
