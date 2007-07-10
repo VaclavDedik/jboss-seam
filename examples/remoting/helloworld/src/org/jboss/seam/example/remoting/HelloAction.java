21fe5d3897339de2a5c4733f3bcdfe6d600de532
Norman Richards <orb@jump.net>
2007-07-10 05:22:53 +0000

    woops

--- examples/remoting/helloworld/src/org/jboss/seam/example/remoting/HelloAction.java
+++ examples/remoting/helloworld/src/org/jboss/seam/example/remoting/HelloAction.java
@@ -3,17 +3,11 @@ package org.jboss.seam.example.remoting;
 import javax.ejb.Stateless;
 
 import org.jboss.seam.annotations.Name;
-import org.jboss.seam.annotations.web.*;
-import org.jboss.seam.annotations.remoting.WebRemote;
 
-//@Stateless
+@Stateless
 @Name("helloAction")
 public class HelloAction implements HelloLocal {
-    @RequestParameter String foo;
-
-    @WebRemote
-    public String sayHello(String name) {
-        System.out.println("Foo=" + foo);
-        return "Hello, " + name;
-    }
+  public String sayHello(String name) {
+    return "Hello, " + name;
+  }
 }
