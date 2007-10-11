2cc18b656535933f357448053ee8ab69f155866c
Pete Muir <pmuir@bleepbleep.org.uk>
2007-10-11 15:24:40 +0000

    Add hibernate identifier test

--- src/test/integration/src/org/jboss/seam/test/integration/IdentifierTest.java
+++ src/test/integration/src/org/jboss/seam/test/integration/IdentifierTest.java
@@ -3,7 +3,9 @@ package org.jboss.seam.test.integration;
 import javax.persistence.EntityManager;
 
 import org.drools.lang.DRLParser.identifier_return;
+import org.hibernate.Session;
 import org.jboss.seam.framework.EntityIdentifier;
+import org.jboss.seam.framework.HibernateEntityIdentifier;
 import org.jboss.seam.mock.SeamTest;
 import org.testng.annotations.Test;
 
@@ -32,6 +34,29 @@ public class IdentifierTest extends SeamTest
                 assert "foo".equals(((Country) entityIdentifier.find(entityManager)).getName());
                 EntityIdentifier entityIdentifier2 = new EntityIdentifier(country, entityManager);
                 assert entityIdentifier.equals(entityIdentifier2);
+            }
+            
+        }.run();
+    }
+    
+    @Test
+    public void testHibernateEntityIdentifier() throws Exception
+    {
+        new ComponentTest()
+        {
+
+            @Override
+            protected void testComponents() throws Exception
+            {
+                setValue("#{countryHome.instance.name}", "foo");
+                invokeMethod("#{countryHome.persist}");
+                Country country = (Country) getValue("#{countryHome.instance}");
+                Session session =  (Session) getValue("#{countryHome.entityManager.delegate}");
+                
+                HibernateEntityIdentifier hibernateEntityIdentifier = new HibernateEntityIdentifier(country, session);
+                assert "foo".equals(((Country) hibernateEntityIdentifier.find(session)).getName());
+                HibernateEntityIdentifier hibernateEntityIdentifier2 = new HibernateEntityIdentifier(country, session);
+                assert hibernateEntityIdentifier.equals(hibernateEntityIdentifier2);
             }
             
         }.run();
