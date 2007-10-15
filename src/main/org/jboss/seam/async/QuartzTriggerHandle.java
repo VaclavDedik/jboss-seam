f2cb8cddcff5e00e1e539ba762e54de4eeda61be
Pete Muir <pmuir@bleepbleep.org.uk>
2007-10-15 10:59:18 +0000

    Hold a transient reference to the scheduler

--- src/main/org/jboss/seam/async/QuartzTriggerHandle.java
+++ src/main/org/jboss/seam/async/QuartzTriggerHandle.java
@@ -2,6 +2,7 @@ package org.jboss.seam.async;
 
 import java.io.Serializable;
 
+import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
 
 /**
@@ -13,24 +14,40 @@ import org.quartz.SchedulerException;
 public class QuartzTriggerHandle implements Serializable
 {
    private String triggerName;
+   
+   // Hold a transient reference to the scheduler to allow control of the
+   // scheduler outside of Seam contexts (useful in a testing context)
+   private transient Scheduler scheduler;
      
    public QuartzTriggerHandle(String triggerName) 
    {
       this.triggerName = triggerName; 
+      this.scheduler = QuartzDispatcher.instance().getScheduler();
    }
 
    public void cancel() throws SchedulerException
    {
-      QuartzDispatcher.instance().getScheduler().unscheduleJob(triggerName, null);
+      getScheduler().unscheduleJob(triggerName, null);
    }
    
    public void pause() throws SchedulerException
    {
-      QuartzDispatcher.instance().getScheduler().pauseTrigger(triggerName, null);  
+      getScheduler().pauseTrigger(triggerName, null);  
    }
    
    public void resume() throws SchedulerException
    {
-      QuartzDispatcher.instance().getScheduler().resumeTrigger(triggerName, null);
+      getScheduler().resumeTrigger(triggerName, null);
    }
+   
+   private Scheduler getScheduler()
+   {
+       if (scheduler == null)
+       {
+           scheduler = QuartzDispatcher.instance().getScheduler();
+       }
+       return scheduler;
+   }
+   
 }
+  
