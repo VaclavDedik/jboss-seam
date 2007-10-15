ae08f745bb6c7e131588aedf9cfa89363ebaf7be
Pete Muir <pmuir@bleepbleep.org.uk>
2007-10-15 10:59:43 +0000

    Needed support classes

---
+++ src/test/integration/src/org/jboss/seam/test/integration/Action.java
@@ -0,0 +1,12 @@
+package org.jboss.seam.test.integration;
+
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Name;
+
+@Name("action")
+public class Action {
+      
+   public String go() {
+      return "success";
+   }
+}
