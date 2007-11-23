6bd632c0d47a6c6b0fcf1c319e84b0ede0449800
Pete Muir <pmuir@bleepbleep.org.uk>
2007-11-23 09:43:19 +0000

    JBSEAM-1081

--- ui/src/main/java/org/jboss/seam/ui/component/UIEnumItem.java
+++ ui/src/main/java/org/jboss/seam/ui/component/UIEnumItem.java
@@ -18,7 +18,31 @@ public abstract class UIEnumItem extends UISelectItem
    public abstract void setLabel(String label);
    
    public abstract String getLabel();
+   
+   @Override
+   public Object getItemValue()
+   {
+      return getEnumValue();
+   }
+   
+   @Override
+   public void setItemValue(Object itemValue)
+   {
+      setEnumValue(itemValue == null ? null : itemValue.toString());
+   }
+   
+   @Override
+   public String getItemLabel()
+   {
+      return getLabel();
+   }
 
+   @Override
+   public void setItemLabel(String itemLabel)
+   {
+      setLabel(itemLabel);
+   }
+   
    @Override
    public Object getValue()
    {
