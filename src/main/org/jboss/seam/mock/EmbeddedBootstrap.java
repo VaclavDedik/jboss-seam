a62e306ad1ec3ed92cd984369f0b296f766384bc
Pete Muir <pmuir@bleepbleep.org.uk>
2007-11-26 18:10:29 +0000

    ws

--- src/main/org/jboss/seam/mock/EmbeddedBootstrap.java
+++ src/main/org/jboss/seam/mock/EmbeddedBootstrap.java
@@ -7,22 +7,25 @@ public class EmbeddedBootstrap
     public void startAndDeployResources() 
         throws Exception
     {
-       Bootstrap bootstrap = Bootstrap.getInstance();
-       bootstrap.bootstrap();
+        Bootstrap bootstrap = Bootstrap.getInstance();
+        bootstrap.bootstrap();
 
-       if (resourceExists("seam.properties")) {
-           bootstrap.deployResourceBases("seam.properties");
-       }
-       if (resourceExists("META-INF/components.xml")) {
-           bootstrap.deployResourceBases("META-INF/components.xml");
-       }
-       if (resourceExists("META-INF/seam.properties")) {
-           bootstrap.deployResourceBases("META-INF/seam.properties");
-       }
-   }
+        if (resourceExists("seam.properties")) 
+        {
+            bootstrap.deployResourceBases("seam.properties");
+        }
+        if (resourceExists("META-INF/components.xml")) 
+        {
+            bootstrap.deployResourceBases("META-INF/components.xml");
+        }
+        if (resourceExists("META-INF/seam.properties")) 
+        {
+            bootstrap.deployResourceBases("META-INF/seam.properties");
+        }
+    }
 
-   private boolean resourceExists(String name)
-   {
-      return Thread.currentThread().getContextClassLoader().getResource(name)!=null;
-   }
+    private boolean resourceExists(String name)
+    {
+        return Thread.currentThread().getContextClassLoader().getResource(name)!=null;
+    }
 }
