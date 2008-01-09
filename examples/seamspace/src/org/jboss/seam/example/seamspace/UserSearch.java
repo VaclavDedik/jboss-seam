52030799a9751daf5172c1227b1646b8f01eb54b
Shane Bryzak <sbryzak@redhat.com>
2008-01-09 10:05:37 +0000

    completed user management screens

---
+++ examples/seamspace/src/org/jboss/seam/example/seamspace/UserSearch.java
@@ -0,0 +1,51 @@
+package org.jboss.seam.example.seamspace;
+
+import static org.jboss.seam.ScopeType.SESSION;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+import org.jboss.seam.annotations.datamodel.DataModel;
+import org.jboss.seam.annotations.datamodel.DataModelSelection;
+import org.jboss.seam.security.management.IdentityManager;
+
+@Name("userSearch")
+@Scope(SESSION)
+public class UserSearch implements Serializable
+{
+   private static final long serialVersionUID = 8592034786339372510L;
+
+   @DataModel
+   List<String> users;
+   
+   @DataModelSelection
+   String selectedUser;
+   
+   @In IdentityManager identityManager;
+   
+   public void loadUsers()
+   {
+      users = identityManager.listUsers();     
+   }
+   
+   public String getUserRoles(String username)
+   {
+      List<String> roles = identityManager.getGrantedRoles(username);
+      StringBuilder sb = new StringBuilder();
+      
+      for (String role : roles)
+      {
+         sb.append((sb.length() > 0 ? ", " : "") + role); 
+      }
+      
+      return sb.toString();      
+   }
+   
+   public String getSelectedUser()
+   {
+      return selectedUser;
+   }
+}
