5038d6b2a650ac61b167248837947037d5e4d206
Pete Muir <pmuir@bleepbleep.org.uk>
2008-01-22 17:25:59 +0000

    JBSEAM-2521 and fixes needed for jbas5 module

--- seam-gen/resources/import.sql
+++
@@ -1,4 +0,0 @@
-insert into UserAccount (accountid, username, enabled, accounttype) values (1, 'admin', 1, 1);
-insert into UserAccount (accountid, username, passwordhash, enabled, accounttype) values (2, 'demo', '04ac53975612d07ad97107ef524589e5', 1, 0);
-
-insert into AccountMembership (accountid, memberof) values (2, 1);
