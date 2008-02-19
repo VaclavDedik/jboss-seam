f4a3b1d7a471a8855db1051dbca1bbb37903b023
Shane Bryzak <sbryzak@redhat.com>
2008-02-19 13:36:02 +0000

    permission action annotations

---
+++ src/main/org/jboss/seam/annotations/security/Insert.java
@@ -0,0 +1,23 @@
+package org.jboss.seam.annotations.security;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/**
+ * 
+ *
+ * @author Shane Bryzak
+ */
+@Target({METHOD})
+@Documented
+@Retention(RUNTIME)
+@Inherited
+@PermissionAction("insert")
+public @interface Insert {
+   Class value();
+}
