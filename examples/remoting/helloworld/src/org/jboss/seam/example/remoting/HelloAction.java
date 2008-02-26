d880b1714408064139031e2971529c4d1005eca0
Shane Bryzak <sbryzak@redhat.com>
2008-02-26 02:09:39 +0000

    simplified

--- examples/remoting/helloworld/src/org/jboss/seam/example/remoting/HelloAction.java
+++ examples/remoting/helloworld/src/org/jboss/seam/example/remoting/HelloAction.java
@@ -1,13 +1,12 @@
 package org.jboss.seam.example.remoting;
 
-import javax.ejb.Stateless;
-
 import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.remoting.WebRemote;
 
-@Stateless
 @Name("helloAction")
-public class HelloAction implements HelloLocal {
-  public String sayHello(String name) {
-    return "Hello, " + name;
-  }
+public class HelloAction {
+   @WebRemote
+   public String sayHello(String name) {
+      return "Hello, " + name;
+   }
 }
