d5cbc4dc8d6e0abdb354301eca59d24a63b64eac
Shane Bryzak <sbryzak@redhat.com>
2008-03-19 23:42:45 +0000

    jpa permission store, consolidated identity-management namespace with security namespace

--- src/main/org/jboss/seam/security/permission/DynamicPermissionResolver.java
+++ src/main/org/jboss/seam/security/permission/DynamicPermissionResolver.java
@@ -6,18 +6,22 @@ import static org.jboss.seam.annotations.Install.FRAMEWORK;
 import java.io.Serializable;
 import java.util.List;
 
+import org.jboss.seam.Component;
 import org.jboss.seam.Seam;
+import org.jboss.seam.annotations.Create;
 import org.jboss.seam.annotations.Install;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
 import org.jboss.seam.annotations.Startup;
 import org.jboss.seam.annotations.intercept.BypassInterceptors;
+import org.jboss.seam.log.LogProvider;
+import org.jboss.seam.log.Logging;
 import org.jboss.seam.security.Identity;
 import org.jboss.seam.security.permission.AccountPermission.AccountType;
 
 /**
- * Resolves permissions dynamically assigned in a persistent store, such as a 
- * database, for example.
+ * Resolves dynamically-assigned permissions kept in a persistent store, such as a 
+ * database.
  * 
  * @author Shane Bryzak
  */
@@ -28,7 +32,31 @@ import org.jboss.seam.security.permission.AccountPermission.AccountType;
 @Startup
 public class DynamicPermissionResolver implements PermissionResolver, Serializable
 {   
+   private static final String DEFAULT_PERMISSION_STORE_NAME = "permissionStore";
+   
    private PermissionStore permissionStore;
+   
+   private static final LogProvider log = Logging.getLogProvider(DynamicPermissionResolver.class);   
+   
+   @Create
+   public void create()
+   {
+      initPermissionStore();
+   }
+   
+   protected void initPermissionStore()
+   {
+      if (permissionStore == null)
+      {
+         permissionStore = (PermissionStore) Component.getInstance(DEFAULT_PERMISSION_STORE_NAME, true);
+      }           
+      
+      if (permissionStore == null)
+      {
+         log.warn("no permission store available - please install a PermissionStore with the name '" +
+               DEFAULT_PERMISSION_STORE_NAME + "' if dynamic permissions are required.");
+      }
+   }     
    
    public PermissionStore getPermissionStore()
    {
--- src/main/org/jboss/seam/security/permission/JPAPermissionStore.java
+++ src/main/org/jboss/seam/security/permission/JPAPermissionStore.java
@@ -4,6 +4,9 @@ import static org.jboss.seam.ScopeType.APPLICATION;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+
+import org.jboss.seam.Component;
 import org.jboss.seam.annotations.Scope;
 import org.jboss.seam.annotations.intercept.BypassInterceptors;
 import org.jboss.seam.security.permission.AccountPermission.AccountType;
@@ -17,26 +20,82 @@ import org.jboss.seam.security.permission.AccountPermission.AccountType;
 @BypassInterceptors
 public class JPAPermissionStore implements PermissionStore
 {
+   private String entityManagerName = "entityManager";
+   
+   private Class<? extends AccountPermission> permissionClass;   
+   
    public boolean grantPermission(String target, String action, String account,
          AccountType accountType) 
    {
-      return false;
-   }
+      try
+      {
+         if (permissionClass == null)
+         {
+            throw new RuntimeException("Could not grant permission, permissionClass not set");
+         }
+                 
+         AccountPermission permission = permissionClass.newInstance();
+         permission.setTarget(target);
+         permission.setAction(action);
+         permission.setAccount(account);
+         permission.setAccountType(accountType);
 
-   public List<AccountPermission> listPermissions(String target, String action) 
-   {
-      return null;
+         getEntityManager().persist(permission);
+         
+         return true;
+      }
+      catch (Exception ex)
+      {
+         throw new RuntimeException("Could not grant permission", ex);
+      }   
    }
-
-   public List<AccountPermission> listPermissions(String target) 
-   {
-      return null;
-   }
-
+   
    public boolean revokePermission(String target, String action,
          String account, AccountType accountType) 
    {
       return false;
+   }   
+
+   public List<AccountPermission> listPermissions(String target, String action) 
+   {
+      return getEntityManager().createQuery(
+            "from " + permissionClass.getName() + 
+            " where target = :target and action = :action")
+            .setParameter("target", target)
+            .setParameter("action", action)
+            .getResultList();
    }
 
+   public List<AccountPermission> listPermissions(String target) 
+   {
+      return getEntityManager().createQuery(
+            "from " + permissionClass.getName() + " where target = :target")
+            .setParameter("target", target)
+            .getResultList();
+   }
+
+   private EntityManager getEntityManager()
+   {
+      return (EntityManager) Component.getInstance(entityManagerName);
+   }
+   
+   public String getEntityManagerName()
+   {
+      return entityManagerName;
+   }
+   
+   public void setEntityManagerName(String name)
+   {
+      this.entityManagerName = name;
+   }      
+   
+   public Class getPermissionClass()
+   {
+      return permissionClass;
+   }
+   
+   public void setPermissionClass(Class permissionClass)
+   {
+      this.permissionClass = permissionClass;
+   }
 }
