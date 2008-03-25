c46b0628686a5ba4b9299a9b66bfb8dcc591f710
Pete Muir <pmuir@bleepbleep.org.uk>
2008-03-25 22:43:41 +0000

    Make jboss-seam.jar actually deploy

--- src/main/org/jboss/seam/security/permission/package-info.java
+++ src/main/org/jboss/seam/security/permission/package-info.java
@@ -3,7 +3,7 @@
  * 
  * @see org.jboss.seam.security.permission.PermissionManager
  */
-@Namespace(value="http://jboss.com/products/seam/security", prefix="org.jboss.seam.security")
+@Namespace(value="http://jboss.com/products/seam/permission", prefix="org.jboss.seam.permission")
 @AutoCreate
 package org.jboss.seam.security.permission;
 
