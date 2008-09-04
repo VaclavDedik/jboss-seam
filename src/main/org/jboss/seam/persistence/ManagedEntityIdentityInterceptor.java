389ec948b8dc93ef78215da3568ed09ffcd0cacd
Pete Muir <pmuir@bleepbleep.org.uk>
2008-09-04 16:17:19 +0000

    ws

--- src/main/org/jboss/seam/persistence/ManagedEntityIdentityInterceptor.java
+++ src/main/org/jboss/seam/persistence/ManagedEntityIdentityInterceptor.java
@@ -10,47 +10,54 @@ import org.jboss.seam.intercept.InvocationContext;
 import org.jboss.seam.transaction.Transaction;
 
 /**
- * Swizzles entity references around each invocation, maintaining
- * referential integrity even across passivation of the stateful 
- * bean or Seam-managed extended persistence context, and allowing 
- * for more efficient replication.
+ * Swizzles entity references around each invocation, maintaining referential
+ * integrity even across passivation of the stateful bean or Seam-managed
+ * extended persistence context, and allowing for more efficient replication.
  * 
  * @author Gavin King
- *
+ * 
  */
-@Interceptor(around=BijectionInterceptor.class)
+@Interceptor(around = BijectionInterceptor.class)
 public class ManagedEntityIdentityInterceptor extends AbstractInterceptor
 {
-    
-    private static ManagedEntityStateManager managedEntityStateManager = new ManagedEntityStateManager();
-   
-    private boolean reentrant;
-    //TODO: cache the non-ignored fields, probably on Component
-    
-    public boolean isInterceptorEnabled()
-    {
-       return getComponent().getScope() == CONVERSATION;
-    }
-   
-    @AroundInvoke
-    public Object aroundInvoke(InvocationContext ctx) throws Exception
-    {
-        if (reentrant) {
+
+   private static ManagedEntityStateManager managedEntityStateManager = new ManagedEntityStateManager();
+
+   private boolean reentrant;
+
+   // TODO: cache the non-ignored fields, probably on Component
+
+   public boolean isInterceptorEnabled()
+   {
+      return getComponent().getScope() == CONVERSATION;
+   }
+
+   @AroundInvoke
+   public Object aroundInvoke(InvocationContext ctx) throws Exception
+   {
+      if (reentrant)
+      {
+         return ctx.proceed();
+      }
+      else
+      {
+         reentrant = true;
+         managedEntityStateManager.entityIdsToRefs(ctx.getTarget(), getComponent());
+         try
+         {
             return ctx.proceed();
-        } else {
-            reentrant = true;
-            managedEntityStateManager.entityIdsToRefs(ctx.getTarget(), getComponent());
-            try  {
-                return ctx.proceed();
-            } finally {
-                if (!isTransactionRolledBackOrMarkedRollback()) {
-                    managedEntityStateManager.entityRefsToIds(ctx.getTarget(), getComponent());
-                    reentrant = false;
-                }
+         }
+         finally
+         {
+            if (!isTransactionRolledBackOrMarkedRollback())
+            {
+               managedEntityStateManager.entityRefsToIds(ctx.getTarget(), getComponent());
+               reentrant = false;
             }
-        }
-    }
-    
+         }
+      }
+   }
+
    private static boolean isTransactionRolledBackOrMarkedRollback()
    {
       try
@@ -62,6 +69,5 @@ public class ManagedEntityIdentityInterceptor extends AbstractInterceptor
          return false;
       }
    }
-   
-   
+
 }
