71833b68e580885649940f97f6835c687c9e1d16
Daniel Roth <danielc.roth@gmail.com>
2008-10-09 20:13:24 +0000

    JBSEAM-3140

--- ui/src/main/java/org/jboss/seam/ui/renderkit/SpanRendererBase.java
+++ ui/src/main/java/org/jboss/seam/ui/renderkit/SpanRendererBase.java
@@ -2,8 +2,11 @@ package org.jboss.seam.ui.renderkit;
 
 import java.io.IOException;
 
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
 import javax.faces.context.ResponseWriter;
 
+import org.jboss.seam.ui.component.UISpan;
 import org.jboss.seam.ui.component.UIStyle;
 
 public class SpanRendererBase extends StyleRendererBase
@@ -12,7 +15,7 @@ public class SpanRendererBase extends StyleRendererBase
    @Override
    protected Class getComponentClass()
    {
-      return UIStyle.class;
+      return UISpan.class;
    }
 
    @Override
@@ -20,6 +23,31 @@ public class SpanRendererBase extends StyleRendererBase
    {
       writer.endElement("span");
    }
+   
+   @Override
+   protected void doEncodeBegin(ResponseWriter writer, FacesContext context, UIComponent component) throws IOException
+   {
+      UISpan span = (UISpan) component;
+      
+      startElement(writer, span);
+
+      writer.writeAttribute("id", component.getClientId(context), "id");
+      
+      if (span.getStyleClass() != null) 
+      {
+         writer.writeAttribute("class", span.getStyleClass(), "styleClass");
+      }
+
+      if (span.getStyle() != null) 
+      {
+         writer.writeAttribute("style", span.getStyle(), "style");
+      }
+
+      if (span.getTitle() != null) 
+      {
+         writer.writeAttribute("title", span.getTitle(), "title");
+      }      
+   }   
    
    @Override
    public void startElement(ResponseWriter writer, UIStyle style) throws IOException
