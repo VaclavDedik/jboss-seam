b3bfedb4a18e9b37676794cefc6118b1be67a419
Daniel Roth <danielc.roth@gmail.com>
2008-10-12 17:00:37 +0000

    JBSEAM-2809

--- examples/ui/src/org/jboss/seam/example/ui/EqualityValidatorBean.java
+++ examples/ui/src/org/jboss/seam/example/ui/EqualityValidatorBean.java
@@ -12,48 +12,28 @@ import org.jboss.seam.util.Strings;
 @Scope(ScopeType.SESSION)
 public class EqualityValidatorBean
 {
-   
+
    private String name;
-   
-   private String nameVerification;
-   
+
    @In
    private StatusMessages statusMessages;
-   
+
    public String getName()
    {
       return name;
    }
-   
+
    public void setName(String name)
    {
       this.name = name;
    }
-   
-   public String getNameVerification()
-   {
-      return nameVerification;
-   }
 
-   public void setNameVerification(String nameVerification)
-   {
-      this.nameVerification = nameVerification;
-   }
-   
    public void check()
    {
       if (Strings.isEmpty(name))
       {
          statusMessages.addToControl("name", Severity.WARN, "Enter a name!");
       }
-      if (Strings.isEmpty(nameVerification))
-      {
-         statusMessages.addToControl("nameVerification", Severity.WARN, "Enter a name verification!");
-      }
-      if (name != null && nameVerification != null && !name.equals(nameVerification))
-      {
-         statusMessages.addToControl("nameVerification", Severity.WARN, "Name and Name Verification not equal (should have been caught by equality validator!)");
-      }
    }
-   
+
 }
