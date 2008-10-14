2e202df96542ae623338780c40e354d822130ec9
Dan Allen <dan.j.allen@gmail.com>
2008-10-14 19:25:43 +0000

    no longer need ToOne

--- src/gen/org/jboss/seam/tool/Util.java
+++ src/gen/org/jboss/seam/tool/Util.java
@@ -3,9 +3,6 @@ package org.jboss.seam.tool;
 import java.util.HashSet;
 import java.util.Set;
 
-import org.hibernate.mapping.Property;
-import org.hibernate.mapping.ToOne;
-
 public class Util
 {
    public String lower(String name)
@@ -19,9 +16,5 @@ public class Util
    public Set set()
    {
       return new HashSet();
-   }
-   public boolean isToOne(Property property)
-   {
-      return (property.getValue() != null) && (property.getValue() instanceof ToOne);
    }
 }
