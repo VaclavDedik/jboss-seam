8e9c56fa7dedeca18c6fd987381b693ebd5047e6
Dan Allen <dan.j.allen@gmail.com>
2008-10-24 03:28:28 +0000

    JBSEAM-3585 (both issues)

--- seam.bat
+++ seam.bat
@@ -1,6 +1,11 @@
 @echo off
-@if not "%ECHO%" == ""  echo %ECHO%
 @if "%OS%" == "Windows_NT" setlocal
+
+rem ##############################################
+rem #                                            #
+rem # seam-gen - Seam project and code generator #
+rem #                                            #
+rem ##############################################
 
 set WORKING_DIR=%CD%
 if "%WORKING_DIR:~-1%" == "\" (
@@ -10,7 +15,7 @@ if "%WORKING_DIR:~-1%" == "\" (
 if ["%SEAM_HOME%"] == [""] (
     set SEAM_HOME=%~dp0
 ) else (
-    if not exist "%SEAM_HOME\seam" (goto noseam)
+    if not exist "%SEAM_HOME\seam.bat" (goto noseam)
 )
 if "%SEAM_HOME:~-1%" == "\" (
     set SEAM_HOME=%SEAM_HOME:~0,-1%
@@ -18,9 +23,6 @@ if "%SEAM_HOME:~-1%" == "\" (
 
 set SEAM_GEN_DIR=%SEAM_HOME%\seam-gen
 set COMMAND=%1%
-
-echo Location of seam script: %SEAM_HOME%
-echo seam-gen template folder: %SEAM_GEN_DIR%
 
 if [%COMMAND%] == [] (goto usage)
 
@@ -30,7 +32,10 @@ if ["%JAVA_HOME%"] == [""] (goto nojava)
 
 if not exist "%JAVA_HOME%\bin\javac.exe" (goto nojdk)
 
-java -cp "%JAVA_HOME%\lib\tools.jar;%SEAM_HOME%\build\lib\ant-launcher.jar;%SEAM_HOME%\build\lib\ant-nodeps.jar;%SEAM_HOME%\build\lib\ant.jar" -Dant.home="%SEAM_HOME%\lib" org.apache.tools.ant.launch.Launcher -buildfile "%SEAM_GEN_DIR%\build.xml" -Dworking.dir="%WORKING_DIR%" %*
+echo SEAM_HOME: %SEAM_HOME%
+echo Using seam-gen sources from: %SEAM_GEN_DIR%
+
+"%JAVA_HOME%\bin\java" -cp "%JAVA_HOME%\lib\tools.jar;%SEAM_HOME%\build\lib\ant-launcher.jar;%SEAM_HOME%\build\lib\ant-nodeps.jar;%SEAM_HOME%\build\lib\ant.jar" -Dant.home="%SEAM_HOME%\lib" org.apache.tools.ant.launch.Launcher -buildfile "%SEAM_GEN_DIR%\build.xml" -Dworking.dir="%WORKING_DIR%" %*
 
 goto END_NO_PAUSE
 
@@ -48,11 +53,11 @@ echo The SEAM_HOME environment variable should point to a Seam distribution
 goto END_NO_PAUSE
 
 :usage
-more %SEAM_GEN_DIR%\USAGE
+more "%SEAM_GEN_DIR%\USAGE"
 goto END_NO_PAUSE
 
 :help
-more %SEAM_GEN_DIR%\README
+more "%SEAM_GEN_DIR%\README"
 goto END_NO_PAUSE
 
 :END_NO_PAUSE
