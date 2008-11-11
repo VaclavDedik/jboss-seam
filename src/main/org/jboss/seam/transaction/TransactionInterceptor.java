94521c4a61adab9cc67d96e80dd0b74a9d3e25d0
Shane Bryzak <sbryzak@redhat.com>
2008-11-11 12:26:23 +0000

    JBSEAM-3519

--- src/main/org/jboss/seam/transaction/TransactionInterceptor.java
+++ src/main/org/jboss/seam/transaction/TransactionInterceptor.java
@@ -4,7 +4,10 @@ import static org.jboss.seam.ComponentType.JAVA_BEAN;
 
 import java.lang.reflect.AnnotatedElement;
 import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.jboss.seam.annotations.TransactionPropagationType;
 import org.jboss.seam.annotations.Transactional;
 import org.jboss.seam.annotations.intercept.AroundInvoke;
 import org.jboss.seam.annotations.intercept.Interceptor;
@@ -19,14 +22,66 @@ import org.jboss.seam.util.Work;
  * Implements transaction propagation rules for Seam JavaBean components.
  * 
  * @author Gavin King
- *
+ * @author Shane Bryzak
  */
-@Interceptor(stateless=true,
+@Interceptor(stateless=false,
              around={RollbackInterceptor.class, BusinessProcessInterceptor.class, 
                      ConversationInterceptor.class, BijectionInterceptor.class})
 public class TransactionInterceptor extends AbstractInterceptor
 {
    private static final long serialVersionUID = -4364203056333738988L;
+   
+   private class TransactionMetadata
+   {
+      private boolean annotationPresent;
+      TransactionPropagationType propType;
+      
+      public TransactionMetadata(AnnotatedElement element)
+      {
+         annotationPresent = element.isAnnotationPresent(Transactional.class);
+         
+         if (annotationPresent)
+         {
+            propType = element.getAnnotation(Transactional.class).value();
+         }
+      }
+      
+      public boolean isAnnotationPresent()
+      {
+         return annotationPresent;
+      }
+      
+      public boolean isNewTransactionRequired(boolean transactionActive)
+      {
+         return propType != null && propType.isNewTransactionRequired(transactionActive);
+      }
+   }
+   
+   private TransactionMetadata lookupTransactionMetadata(AnnotatedElement element)
+   {
+      TransactionMetadata metadata = transactionMetadata.get(element);
+      
+      if (metadata == null)
+      {
+         metadata = loadMetadata(element);
+      }
+      
+      return metadata;
+   }
+   
+   private synchronized TransactionMetadata loadMetadata(AnnotatedElement element)
+   {
+      if (!transactionMetadata.containsKey(element))
+      {
+         TransactionMetadata metadata = new TransactionMetadata(element);
+         transactionMetadata.put(element, metadata);
+         return metadata;
+      }
+      
+      return transactionMetadata.get(element);
+   }
+   
+   private Map<AnnotatedElement,TransactionMetadata> transactionMetadata = new HashMap<AnnotatedElement, TransactionMetadata>();
    
    @AroundInvoke
    public Object aroundInvoke(final InvocationContext invocation) throws Exception
@@ -48,19 +103,16 @@ public class TransactionInterceptor extends AbstractInterceptor
          
          private boolean isNewTransactionRequired(Method method, Class beanClass, boolean transactionActive)
          {
-            return isTransactionAnnotationPresent(method) ? 
-                  isNewTransactionRequired(method, transactionActive) :
-                  isTransactionAnnotationPresent(beanClass) && isNewTransactionRequired(beanClass, transactionActive);
-         }
-         
-         private boolean isTransactionAnnotationPresent(AnnotatedElement element)
-         {
-            return element.isAnnotationPresent(Transactional.class);
-         }
-         
-         private boolean isNewTransactionRequired(AnnotatedElement element, boolean transactionActive)
-         {
-            return element.getAnnotation(Transactional.class).value().isNewTransactionRequired(transactionActive);
+            TransactionMetadata metadata = lookupTransactionMetadata(method);
+            if (metadata.isNewTransactionRequired(transactionActive))
+            {
+               return true;
+            }
+            else
+            {
+               metadata = lookupTransactionMetadata(beanClass);
+               return metadata.isNewTransactionRequired(transactionActive);
+            }
          }
          
       }.workInTransaction();      
