453bfa7e1429079986cc4492bde61c5dac5e1b38
Norman Richards <orb@nostacktrace.com>
2009-01-12 18:40:23 +0000

    JBSEAM-3799

--- examples/mail/src/org/jboss/seam/example/mail/AsynchronousMailProcessor.java
+++ examples/mail/src/org/jboss/seam/example/mail/AsynchronousMailProcessor.java
@@ -6,18 +6,21 @@ import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.async.Duration;
 import org.jboss.seam.contexts.Contexts;
 import org.jboss.seam.faces.Renderer;
+import org.jboss.seam.log.*;
 
 @Name("asynchronousMailProcessor")
 @AutoCreate
 public class AsynchronousMailProcessor
 {
-   @Asynchronous
-   public void scheduleSend(@Duration long delay, Person person) {
-      try {
-         Contexts.getEventContext().set("person", person);
-         Renderer.instance().render("/simple.xhtml");
-      } catch (Exception e) {
-         e.printStackTrace();
-      }
-   }
+    private static final LogProvider log = Logging.getLogProvider(AsynchronousMailProcessor.class); 
+
+    @Asynchronous
+    public void scheduleSend(@Duration long delay, Person person) {
+	try {
+	    Contexts.getEventContext().set("person", person);
+	    Renderer.instance().render("/simple.xhtml");
+	} catch (Exception e) {
+	    log.error("Error scheduling send #0",e);
+	}
+    }
 }
