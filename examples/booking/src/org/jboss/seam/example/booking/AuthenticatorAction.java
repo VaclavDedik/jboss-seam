03ab2d2d20dbec287e23780957b1e79db3435174
Norman Richards <orb@nostacktrace.com>
2009-04-01 00:37:47 +0000

    JBSEAM-4015

--- examples/booking/src/org/jboss/seam/example/booking/AuthenticatorAction.java
+++ examples/booking/src/org/jboss/seam/example/booking/AuthenticatorAction.java
@@ -8,33 +8,33 @@ import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Out;
 
 @Stateless
 @Name("authenticator")
-public class AuthenticatorAction implements Authenticator
+public class AuthenticatorAction 
+    implements Authenticator
 {
-   @PersistenceContext 
-   private EntityManager em;
-   
-   @Out(required=false, scope = SESSION)
-   private User user;
-   
-   public boolean authenticate()
-   {
-      List results = em.createQuery("select u from User u where u.username=#{identity.username} and u.password=#{identity.password}")
-            .getResultList();
-      
-      if ( results.size()==0 )
-      {
-         return false;
-      }
-      else
-      {
-         user = (User) results.get(0);
-         return true;
-      }
-   }
+    @PersistenceContext 
+    private EntityManager em;
 
+    @In(required=false)   
+    @Out(required=false, scope = SESSION)
+    private User user;
+   
+    public boolean authenticate()
+    {
+	List results = em.createQuery("select u from User u where u.username=#{identity.username} and u.password=#{identity.password}")
+                         .getResultList();
+      
+	if (results.size()==0) {
+	    return false;
+	} else {
+	    user = (User) results.get(0);
+	    return true;
+	}
+    }
+    
 }
