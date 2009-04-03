714d45c1f232e894029188c6404f2a57f4cd9ccf
Dan Allen <dan.j.allen@gmail.com>
2009-04-03 06:14:04 +0000

    JBSEAM-3361 roll back all changes to BijectionInterceptor in 2.0 branch; too big a change for a point release

--- src/main/org/jboss/seam/core/BijectionInterceptor.java
+++ src/main/org/jboss/seam/core/BijectionInterceptor.java
@@ -1,9 +1,6 @@
 //$Id$
 package org.jboss.seam.core;
 
-import java.lang.reflect.Method;
-import java.util.concurrent.locks.ReentrantLock;
-
 import org.jboss.seam.Component;
 import org.jboss.seam.annotations.intercept.AroundInvoke;
 import org.jboss.seam.annotations.intercept.Interceptor;
@@ -11,128 +8,52 @@ import org.jboss.seam.intercept.AbstractInterceptor;
 import org.jboss.seam.intercept.InvocationContext;
 import org.jboss.seam.log.LogProvider;
 import org.jboss.seam.log.Logging;
-import org.jboss.seam.util.Reflections;
 
 /**
  * Before invoking the component, inject all dependencies. After
  * invoking, outject dependencies back into their context.
  * 
  * @author Gavin King
- * @author Shane Bryzak
  */
 @Interceptor
 public class BijectionInterceptor extends AbstractInterceptor
 {
    private static final long serialVersionUID = 4686458105931528659L;
-
+   
    private static final LogProvider log = Logging.getLogProvider(BijectionInterceptor.class);
    
-   private boolean injected;
-   
-   private boolean injecting;
-   
-   private int counter = 0;
-   
-   private ReentrantLock lock = new ReentrantLock();
-         
-   private String initialMethod;
+   private boolean reentrant; //OK, since all Seam components are single-threaded
    
    @AroundInvoke
    public Object aroundInvoke(InvocationContext invocation) throws Exception
    {
-      Component component = getComponent();    
-      Method method = invocation.getMethod();
-      boolean enforceRequired = !component.isLifecycleMethod( method );      
-      
-      try
-      {    
-         lock.lock();
-         try
+      if (reentrant)
+      {
+         if ( log.isTraceEnabled() )
          {
-            if (!injected)
-            {              
-               if (injecting)
-               {
-                  log.warn("Injecting dependencies into " + component.getName() + " for the invocation of " 
-                        + initialMethod + " caused the invocation of a reentrant method: " + Reflections.toString(method) 
-                        + ".  Some injected dependencies may not be available for the duration of this method invocation.");
-               }
-               else
-               {
-                  injecting = true;
-                  try
-                  {
-                     initialMethod = Reflections.toString(method);
-                     component.inject(invocation.getTarget(), enforceRequired);
-                  }
-                  finally
-                  {
-                     injecting = false;
-                     initialMethod = null;
-                  }
-               }
-               injected = true;
-            }
-            
-            counter++;
+            log.trace("reentrant call to component: " + getComponent().getName() );
          }
-         finally
-         {
-            lock.unlock();
-         } 
-                           
-         Object result = invocation.proceed();
-            
-         lock.lock();
-         try
-         {
-            counter--;
-            
-            if (counter == 0)
-            {
-               try
-               {                     
-                  component.outject( invocation.getTarget(), enforceRequired );
-               }
-               finally
-               {
-                  // Avoid an extra lock by disinjecting here instead of the finally block
-                  if (injected)
-                  {
-                     injected = false;
-                     component.disinject( invocation.getTarget() );
-                  }
-               }   
-            }
-         }
-         finally
-         {
-            lock.unlock();
-         }
-         
-         return result;
+         return invocation.proceed();
       }
-      finally
-      {            
-         if (injected)
+      else
+      {
+         reentrant = true;
+         try
          {
-            lock.lock();
-            try
-            {
-               counter--;
-               
-               if (counter == 0)
-               {
-                  injected = false;
-                  component.disinject( invocation.getTarget() );     
-               }
-            }
-            finally
-            {
-               lock.unlock();
-            }
+            Component component = getComponent();
+            boolean enforceRequired = !component.isLifecycleMethod( invocation.getMethod() );
+            component.inject( invocation.getTarget(), enforceRequired );
+            Object result = invocation.proceed();            
+            component.outject( invocation.getTarget(), enforceRequired );
+            component.disinject( invocation.getTarget() );
+            return result;
+            
+         }
+         finally
+         {
+            reentrant = false;
          }
       }
    }
-   
+
 }
