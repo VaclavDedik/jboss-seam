7b1b294659434892011d124a693f98eff2df117d
Marek Novotny <hotmana76@gmail.com>
2011-10-28 17:36:08 +0000

    JBSEAM-4840 aplied JAVASSIST fix for JASSIST-66

--- jboss-seam/src/main/java/org/jboss/seam/util/ProxyFactory.java
+++ jboss-seam/src/main/java/org/jboss/seam/util/ProxyFactory.java
@@ -860,6 +860,9 @@ public class ProxyFactory {
         code.addGetstatic(thisClassName, HOLDER, HOLDER_TYPE);
         code.addAstore(arrayVar);
         code.addAload(arrayVar);
+        
+        callFind2Methods(code, meth.getName(), delegatorName, origIndex, desc, arrayVar);
+        /*
         code.addIconst(origIndex);
         code.addOpcode(Opcode.AALOAD);
         code.addOpcode(Opcode.IFNONNULL);
@@ -870,7 +873,7 @@ public class ProxyFactory {
         callFindMethod(code, "findMethod", arrayVar, delIndex, delegatorName, desc);
 
         int pc2 = code.currentPc();
-        code.write16bit(pc, pc2 - pc + 1);
+        code.write16bit(pc, pc2 - pc + 1);*/
         code.addAload(0);
         code.addGetfield(thisClassName, HANDLER, HANDLER_TYPE);
         code.addAload(0);
@@ -893,10 +896,10 @@ public class ProxyFactory {
 
         CodeAttribute ca = code.toCodeAttribute();
         forwarder.setCodeAttribute(ca);
-        StackMapTable.Writer writer = new StackMapTable.Writer(32);
+        /*StackMapTable.Writer writer = new StackMapTable.Writer(32);
         writer.appendFrame(pc2, new int[] { StackMapTable.OBJECT },
                            new int[] { cp.addClassInfo(HOLDER_TYPE) });
-        ca.setAttribute(writer.toStackMapTable(cp));
+        ca.setAttribute(writer.toStackMapTable(cp));*/
         return forwarder;
     }
 
@@ -994,6 +997,27 @@ public class ProxyFactory {
             code.addOpcode(Opcode.AASTORE);
         }
     }
+
+    private static void callFind2Methods(Bytecode code, String superMethod, String thisMethod,
+          int index, String desc, int arrayVar) 
+    {
+      String findClass = RuntimeSupport.class.getName();
+      String findDesc = "(Ljava/lang/Object;Ljava/lang/String;Ljava/lang/String;ILjava/lang/String;[Ljava/lang/reflect/Method;)V";
+      code.addAload(0);
+      code.addLdc(superMethod);
+      if (thisMethod == null)
+      {
+         code.addOpcode(Opcode.ACONST_NULL);
+      }
+      else
+      {
+         code.addLdc(thisMethod);
+      }
+      code.addIconst(index);
+      code.addLdc(desc);
+      code.addAload(arrayVar);
+      code.addInvokestatic(findClass, "find2Methods", findDesc);
+   }
 
     private static int makeWrapper(Bytecode code, Class type, int regno) {
         int index = FactoryHelper.typeIndex(type);
