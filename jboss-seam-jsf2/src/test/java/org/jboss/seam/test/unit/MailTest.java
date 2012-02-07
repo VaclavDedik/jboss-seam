082e002f15bdad109496bbe1021273539a20a4ce
Marek Novotny <hotmana76@gmail.com>
2012-02-07 14:40:58 +0000

    Removed Meldware from code

--- jboss-seam-jsf2/src/test/java/org/jboss/seam/test/unit/MailTest.java
+++ jboss-seam-jsf2/src/test/java/org/jboss/seam/test/unit/MailTest.java
@@ -1,13 +1,10 @@
 package org.jboss.seam.test.unit;
 
-import java.util.ArrayList;
-
 import javax.mail.NoSuchProviderException;
 import javax.mail.Session;
 import javax.naming.NamingException;
 
 import org.jboss.seam.mail.MailSession;
-import org.jboss.seam.mail.MeldwareUser;
 import org.testng.annotations.Test;
 
 import com.sun.mail.smtp.SMTPSSLTransport;
@@ -366,36 +363,5 @@ public class MailTest
       
       assert failure;
    }
-   
-   @Test
-   public void testMeldwareUser()
-   {
-      MeldwareUser meldwareUser = new MeldwareUser();
-      meldwareUser.setUsername(USERNAME);
-      meldwareUser.setPassword(PASSWORD);
-      meldwareUser.getAliases().add(EMAIL);
-      
-      assert USERNAME.equals(meldwareUser.getUsername());
-      assert PASSWORD.equals(meldwareUser.getPassword());
-      assert meldwareUser.getAliases() != null;
-      assert meldwareUser.getAliases().contains(EMAIL);
-      assert meldwareUser.getRoles().contains("calendaruser");
-      assert !meldwareUser.getRoles().contains("adminuser");
-      
-      meldwareUser.setAliases(new ArrayList<String>());
-      assert meldwareUser.getAliases().isEmpty();
-   }
-   
-   @Test
-   public void testAdminMeldwareUser()
-   {
-      MeldwareUser meldwareUser = new MeldwareUser();
-      meldwareUser.setAdministrator(true);
-      
-      assert meldwareUser.getRoles().contains("calendaruser");
-      assert meldwareUser.getRoles().contains("adminuser");
-   }
-   
-   // TODO Write tests for Meldware
 
 }
