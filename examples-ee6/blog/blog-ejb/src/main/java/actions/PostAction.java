069dfa568feecc68f52accd6885459beb58650fc
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2012-03-15 21:27:20 +0000

    added svn:ignore for blog dir

---
+++ examples-ee6/blog/blog-ejb/src/main/java/actions/PostAction.java
@@ -0,0 +1,50 @@
+package actions;
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.persistence.EntityManager;
+
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+import org.jboss.seam.cache.CacheProvider;
+import org.jboss.seam.faces.FacesMessages;
+
+import domain.Blog;
+import domain.BlogEntry;
+
+/**
+ * Handles submission of a new blog entry
+ * 
+ * @author Gavin King
+ */
+@Name("postAction")
+@Scope(ScopeType.STATELESS)
+public class PostAction
+{
+   @In Blog blog;
+   
+   @In EntityManager entityManager;
+   
+   @In(required=false) BlogEntry blogEntry;
+   
+   @In CacheProvider cacheProvider;
+
+   @In FacesMessages facesMessages;
+
+   public void post() throws IOException
+   {
+      blogEntry.setDate( new Date() );
+      blog.getBlogEntries().add(blogEntry);
+      entityManager.persist(blogEntry);
+      cacheProvider.remove("pageFragments", "index");
+   }
+   
+   public void invalid()
+   {
+      facesMessages.add("You are missing some information, please try again");
+   }
+   
+}
