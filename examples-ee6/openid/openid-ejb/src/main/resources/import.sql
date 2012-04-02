82db7eb2dc73efccb9d572b57f9e7086c119ab0f
Marek Schmidt <maschmid@redhat.com>
2012-04-02 15:01:30 +0000

    add openid to the ee6 examples

---
+++ examples-ee6/openid/openid-ejb/src/main/resources/import.sql
@@ -0,0 +1,2 @@
+insert into post (id,creator,message,createDate) values (-1,'http://maximoburrito.myopenid.com/','First Post!',now());
+insert into post (id,creator,message,createDate) values (-2,'http://maximoburrito.myopenid.com/','Have fun with Seam!',now());
