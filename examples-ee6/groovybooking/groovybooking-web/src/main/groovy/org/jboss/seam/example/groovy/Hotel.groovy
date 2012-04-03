377aa624f30e138433c438e592a9100cb4c341d2
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2012-04-03 16:24:40 +0000

    groovybooking migrated in, still not functional, does compile, test, not integration-test

---
+++ examples-ee6/groovybooking/groovybooking-web/src/main/groovy/org/jboss/seam/example/groovy/Hotel.groovy
@@ -0,0 +1,47 @@
+//$Id: Hotel.groovy 4698 2007-04-18 06:40:06Z ebernard $
+package org.jboss.seam.example.groovy
+
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+
+import org.jboss.seam.annotations.Name
+import javax.validation.constraints.NotNull
+import javax.validation.constraints.Size
+
+
+@Entity
+@Name("hotel")
+class Hotel implements Serializable
+{
+   @Id @GeneratedValue
+   Long id
+
+   @Size(max=50) @NotNull
+   String name
+
+   @Size(max=100) @NotNull
+   String address
+
+   @Size(max=40) @NotNull
+   String city
+
+   @Size(min=2, max=10) @NotNull
+   String state
+
+   @Size(min=4, max=6) @NotNull
+   String zip
+
+   @Size(min=2, max=40) @NotNull
+   String country
+
+   @Column(precision=6, scale=2)
+   BigDecimal price
+
+   @Override
+   String toString()
+   {
+      return "Hotel(${name},${address},${city},${zip})"
+   }
+}
