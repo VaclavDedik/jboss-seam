0df0e547514ca4696a35909a127806287aa60fc9
Marek Schmidt <maschmid@redhat.com>
2012-04-16 11:49:54 +0000

    contactlist example deseamtested

--- examples-ee6/contactlist/contactlist-tests/src/test/java/org/jboss/seam/example/contactlist/test/ContactListTest.java
+++ examples-ee6/contactlist/contactlist-tests/src/test/java/org/jboss/seam/example/contactlist/test/ContactListTest.java
@@ -5,16 +5,20 @@ import java.util.List;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.OverProtocol;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.seam.Component;
+import org.jboss.seam.contexts.Lifecycle;
 import org.jboss.seam.example.contactlist.Contact;
-import org.jboss.seam.mock.JUnitSeamTest;
+import org.jboss.seam.framework.EntityHome;
+import org.jboss.seam.framework.EntityQuery;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.spec.EnterpriseArchive;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import static org.junit.Assert.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(Arquillian.class)
-public class ContactListTest extends JUnitSeamTest
+public class ContactListTest
 {
 
    @Deployment(name="ContactListTest")
@@ -25,10 +29,6 @@ public class ContactListTest extends JUnitSeamTest
       WebArchive web = er.getAsType(WebArchive.class, "contactlist-web.war");
 
       web.addClasses(ContactListTest.class);
-      
-      // Install org.jboss.seam.mock.MockSeamListener
-      web.delete("/WEB-INF/web.xml");
-      web.addAsWebInfResource("web.xml");
 
       return er;
    }
@@ -36,141 +36,64 @@ public class ContactListTest extends JUnitSeamTest
    @Test
    public void testList() throws Exception
    {
-      new NonFacesRequest("/search.xhtml")
-      {
-         @Override
-         protected void renderResponse() throws Exception
-         {
-            List<Contact> contacts = (List<Contact>) getValue("#{contacts.resultList}");
-            assert contacts.size()==5;
-         }
-      }.run();
+      EntityQuery<Contact> contacts = (EntityQuery<Contact>)Component.getInstance("contacts");
+      List<Contact> contactsList = (List<Contact>) (contacts.getResultList());
+      assertEquals(5, contactsList.size());
    }
 
    @Test
    public void testSearch() throws Exception
    {
-      new FacesRequest("/search.xhtml")
-      {
-         @Override
-         protected void updateModelValues() throws Exception
-         {
-            setValue("#{exampleContact.firstName}", "Norman");
-         }
-         @Override
-         protected void invokeApplication() throws Exception
-         {
-            setOutcome("/search.xhtml");
-         }
-         @Override
-         protected void afterRequest()
-         {
-            assert !isRenderResponseBegun();
-         }
-      }.run();
-      
-      new NonFacesRequest("/search.xhtml")
-      {         
-         @Override
-         protected void beforeRequest()
-         {
-            setParameter("firstName", "Norman");
-         }
-         @Override
-         protected void renderResponse() throws Exception
-         {
-            List<Contact> contacts = (List<Contact>) getValue("#{contacts.resultList}");
-            System.out.println(contacts.size());
-            assert contacts.size()==1;
-         }
-      }.run();
+      Contact exampleContact = (Contact)Component.getInstance("exampleContact");
+      exampleContact.setFirstName("Norman");
 
-      new FacesRequest("/search.xhtml")
-      {
-         @Override
-         protected void updateModelValues() throws Exception
-         {
-            setValue("#{exampleContact.lastName}", "King");
-         }
-         @Override
-         protected void invokeApplication() throws Exception
-         {
-            setOutcome("/search.xhtml");
-         }
-         @Override
-         protected void afterRequest()
-         {
-            assert !isRenderResponseBegun();
-         }
-      }.run();
+      EntityQuery<Contact> contacts = (EntityQuery<Contact>)Component.getInstance("contacts");
+      List<Contact> contactsList = (List<Contact>) (contacts.getResultList());
+      assertEquals(1, contactsList.size());
       
-      new NonFacesRequest("/search.xhtml")
-      {         
-         @Override
-         protected void beforeRequest()
-         {
-            setParameter("lastName", "King");
-         }
-         @Override
-         protected void renderResponse() throws Exception
-         {
-            List<Contact> contacts = (List<Contact>) getValue("#{contacts.resultList}");
-            assert contacts.size()==1;
-         }
-      }.run();
+      Lifecycle.endCall();
+
+      Lifecycle.beginCall();
+
+      exampleContact = (Contact)Component.getInstance("exampleContact");
+      exampleContact.setLastName("King");
+      
+      contacts = (EntityQuery<Contact>)Component.getInstance("contacts");
+      contactsList = (List<Contact>) (contacts.getResultList());
+      assertEquals(1, contactsList.size());
+      
+      Lifecycle.endCall();
    }
    
-   String contactId;
+   Long contactId;
    
    @Test
    public void testCreateDeleteContact() throws Exception
    {
-      new FacesRequest("/editContact.xhtml")
-      {
-         @Override
-         protected void updateModelValues() throws Exception
-         {
-            setValue("#{contact.firstName}", "Emmanuel");
-            setValue("#{contact.lastName}", "Bernard");
-            setValue("#{contact.city}", "Paris");
-         }
-         @Override
-         protected void invokeApplication() throws Exception
-         {
-            assert invokeMethod("#{contactHome.persist}").equals("persisted");
-            contactId = getValue("#{contactHome.id}").toString();
-         }
-      }.run();
+      Contact contact = (Contact) Component.getInstance("contact");
+      contact.setFirstName("Emmanuel");
+      contact.setLastName("Bernard");
+      contact.setCity("Paris");
       
-      new NonFacesRequest("/viewContact.xhtml")
-      {
-         @Override
-         protected void beforeRequest()
-         {
-            setParameter("contactId", contactId);
-         }
-         @Override
-         protected void renderResponse() throws Exception
-         {
-            assert getValue("#{contact.firstName}").equals("Emmanuel");
-            assert getValue("#{contact.lastName}").equals("Bernard");
-            assert getValue("#{contact.city}").equals("Paris");
-         }
-      }.run();
-      
-      new FacesRequest("/viewContact.xhtml")
-      {
-         @Override
-         protected void beforeRequest()
-         {
-            setPageParameter( "contactId", new Long(contactId) );
-         }
-         @Override
-         protected void invokeApplication() throws Exception
-         {
-            assert invokeMethod("#{contactHome.remove}").equals("removed");
-         }
-      }.run();
-   }
+      EntityHome<Contact> contactHome = (EntityHome<Contact>)Component.getInstance("contactHome");
+      assertEquals("persisted", contactHome.persist());
+      contactId = (Long)contactHome.getId();
 
+      Lifecycle.endCall();
+      Lifecycle.beginCall();
+
+      contactHome = (EntityHome<Contact>)Component.getInstance("contactHome");
+      contactHome.setId(contactId);
+      contact = (Contact) Component.getInstance("contact");
+      assertEquals("Emmanuel", contact.getFirstName());
+      assertEquals("Bernard", contact.getLastName());
+      assertEquals("Paris", contact.getCity());
+
+      Lifecycle.endCall();
+      Lifecycle.beginCall();
+      
+      contactHome = (EntityHome<Contact>)Component.getInstance("contactHome");
+      contactHome.setId(contactId);
+      assertEquals("removed", contactHome.remove());
+   }
 }
