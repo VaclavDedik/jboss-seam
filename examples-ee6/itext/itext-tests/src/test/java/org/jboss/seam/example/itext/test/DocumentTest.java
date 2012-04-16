81a739f86122ae07b9484bd95c24207cc455c35f
Marek Schmidt <maschmid@redhat.com>
2012-04-16 14:28:04 +0000

    itext seamtest

--- examples-ee6/itext/itext-tests/src/test/java/org/jboss/seam/example/itext/test/DocumentTest.java
+++ examples-ee6/itext/itext-tests/src/test/java/org/jboss/seam/example/itext/test/DocumentTest.java
@@ -1,22 +1,50 @@
 package org.jboss.seam.example.itext.test;
 
+import java.io.File;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.OverProtocol;
+import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.seam.contexts.Contexts;
 import org.jboss.seam.core.Conversation;
 import org.jboss.seam.document.ByteArrayDocumentData;
 import org.jboss.seam.document.DocumentData;
 import org.jboss.seam.document.DocumentStore;
-import org.jboss.seam.mock.SeamTest;
+import org.jboss.seam.mock.JUnitSeamTest;
 import org.jboss.seam.pdf.ui.UIDocument;
-import org.junit.Ignore;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.StringAsset;
+import org.jboss.shrinkwrap.api.importer.ZipImporter;
+import org.jboss.shrinkwrap.api.spec.EnterpriseArchive;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /**
  * This is just a placeholder until I can find a way to actually test the UI
  * components.
  */
-@Ignore
-public class DocumentTests extends SeamTest
+@RunWith(Arquillian.class)
+public class DocumentTest extends JUnitSeamTest
 {
+   @Deployment
+   @OverProtocol("Servlet 3.0")
+   public static Archive<?> createDeployment()
+   {
+       EnterpriseArchive er = ShrinkWrap.create(ZipImporter.class, "seam-itext.ear").importFrom(new File("../itext-ear/target/seam-itext.ear"))
+           .as(EnterpriseArchive.class);
+       WebArchive web = er.getAsType(WebArchive.class, "itext-web.war");
+       web.addAsWebInfResource(new StringAsset("org.jboss.seam.mock.MockFacesContextFactory"), "classes/META-INF/services/javax.faces.context.FacesContextFactory");
+       web.addClasses(DocumentTest.class);
+       
+       // Install org.jboss.seam.mock.MockSeamListener
+       web.delete("/WEB-INF/web.xml");
+       web.addAsWebInfResource("web.xml");
+
+       return er;
+   }
+   
    @Test
    public void documentStore() throws Exception
    {
