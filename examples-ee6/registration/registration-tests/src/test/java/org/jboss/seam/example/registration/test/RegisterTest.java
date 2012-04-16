6687c4ec68a4a5bcdeb894067b9dadf81b073534
Marek Schmidt <maschmid@redhat.com>
2012-04-16 16:06:53 +0000

    registration seamtest fix

--- examples-ee6/registration/registration-tests/src/test/java/org/jboss/seam/example/registration/test/RegisterTest.java
+++ examples-ee6/registration/registration-tests/src/test/java/org/jboss/seam/example/registration/test/RegisterTest.java
@@ -1,15 +1,40 @@
 //$Id: RegisterTest.java 10428 2009-04-15 21:54:38Z norman.richards@jboss.com $
 package org.jboss.seam.example.registration.test;
 
+import java.io.File;
+
 import javax.faces.context.FacesContext;
 
-import org.jboss.seam.mock.SeamTest;
-import org.testng.annotations.Test;
-import org.junit.Ignore;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.OverProtocol;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.seam.mock.JUnitSeamTest;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.importer.ZipImporter;
+import org.jboss.shrinkwrap.api.spec.EnterpriseArchive;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
-@Ignore
-public class RegisterTest extends SeamTest
+@RunWith(Arquillian.class)
+public class RegisterTest extends JUnitSeamTest
 {
+   @Deployment(name="RegisterTest")
+   @OverProtocol("Servlet 3.0") 
+   public static Archive<?> createDeployment()
+   {
+     EnterpriseArchive er = ShrinkWrap.create(ZipImporter.class, "seam-registration.ear").importFrom(new File("../registration-ear/target/seam-registration.ear"))
+               .as(EnterpriseArchive.class);
+     WebArchive web = er.getAsType(WebArchive.class, "registration-web.war");
+     web.addClasses(RegisterTest.class);
+
+     // Install org.jboss.seam.mock.MockSeamListener
+     web.delete("/WEB-INF/web.xml");
+     web.addAsWebInfResource("web.xml");
+     
+     return er;
+  }
    
    @Test
    public void testLogin() throws Exception
