6b45966c0d89b09a7f5d1c5a7c301e98664b2a90
Marek Schmidt <maschmid@redhat.com>
2012-04-17 10:07:32 +0000

    restbay example, change Category id generation strategy to Identity, so it works with the current import.sql script

--- examples-ee6/restbay/restbay-ejb/src/main/java/org/jboss/seam/example/restbay/Category.java
+++ examples-ee6/restbay/restbay-ejb/src/main/java/org/jboss/seam/example/restbay/Category.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -20,7 +21,7 @@ public class Category implements Serializable
    private Category parent;
    
    @Id
-   @GeneratedValue
+   @GeneratedValue(strategy=GenerationType.IDENTITY)
    public Integer getCategoryId()
    {
       return categoryId;
