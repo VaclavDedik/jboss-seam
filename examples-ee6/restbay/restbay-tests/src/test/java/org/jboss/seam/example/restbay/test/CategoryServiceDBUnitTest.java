d545b5d95ff041d252eee11320093d039ab4258d
Marek Schmidt <maschmid@redhat.com>
2012-04-19 15:28:21 +0000

    restbay CategoryServiceDBUnitTest

--- examples-ee6/restbay/restbay-tests/src/test/java/org/jboss/seam/example/restbay/test/CategoryServiceDBUnitTest.java
+++ examples-ee6/restbay/restbay-tests/src/test/java/org/jboss/seam/example/restbay/test/CategoryServiceDBUnitTest.java
@@ -1,15 +1,23 @@
 package org.jboss.seam.example.restbay.test;
 
 import org.dbunit.operation.DatabaseOperation;
-import org.testng.annotations.Test;
-import org.testng.annotations.BeforeClass;
-import org.jboss.seam.mock.DBUnitSeamTest;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.OverProtocol;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.seam.mock.DBJUnitSeamTest;
 
 import org.jboss.seam.mock.ResourceRequestEnvironment;
 import org.jboss.seam.mock.EnhancedMockHttpServletRequest;
 import org.jboss.seam.mock.EnhancedMockHttpServletResponse;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.spec.EnterpriseArchive;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.resolver.api.DependencyResolvers;
+import org.jboss.shrinkwrap.resolver.api.maven.MavenDependencyResolver;
 
-import org.junit.Ignore;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.jboss.seam.mock.ResourceRequestEnvironment.ResourceRequest;
 import static org.jboss.seam.mock.ResourceRequestEnvironment.Method;
@@ -20,14 +28,34 @@ import java.util.HashMap;
 /**
  *
  */
-@Ignore
-public class CategoryServiceDBUnitTest extends DBUnitSeamTest
+@RunWith(Arquillian.class)
+public class CategoryServiceDBUnitTest extends DBJUnitSeamTest
 {
+   @Deployment(name="CategoryServiceDBUnitTest")
+   @OverProtocol("Servlet 3.0")
+   public static Archive<?> createDeployment()
+   {
+      EnterpriseArchive er = Deployments.restbayDeployment();
+      WebArchive web = er.getAsType(WebArchive.class, "restbay-web.war");
+      er.addAsLibraries(DependencyResolvers.use(MavenDependencyResolver.class)
+            .configureFrom("pom.xml")
+            .artifact("org.dbunit:dbunit:jar:2.2")
+            .resolveAsFiles());
+      
+      web.addAsResource("org/jboss/seam/example/restbay/test/dbunitdata.xml", "org/jboss/seam/example/restbay/test/dbunitdata.xml");
+      
+      web.addClasses(CategoryServiceDBUnitTest.class);
+      return er;
+   }
 
    protected void prepareDBUnitOperations() {
-       beforeTestOperations.add(
-               new DataSetOperation("org/jboss/seam/example/restbay/test/dbunitdata.xml", DatabaseOperation.CLEAN_INSERT)
-       );
+      
+      setDatabase("hsql");
+      setDatasourceJndiName("java:/restbayDatasource");
+      
+      beforeTestOperations.add(
+            new DataSetOperation("org/jboss/seam/example/restbay/test/dbunitdata.xml", DatabaseOperation.CLEAN_INSERT)
+      );
    }
 
    // Or, if you don't want shared headers between test methods, just use
@@ -36,7 +64,7 @@ public class CategoryServiceDBUnitTest extends DBUnitSeamTest
    // new ResourceRequest(new ResourceRequestTest(this), Method.GET, ...).run();
 
    ResourceRequestEnvironment sharedEnvironment;
-   @BeforeClass
+   @Before
    public void prepareSharedEnvironment() throws Exception
    {
       sharedEnvironment = new ResourceRequestEnvironment(this)
