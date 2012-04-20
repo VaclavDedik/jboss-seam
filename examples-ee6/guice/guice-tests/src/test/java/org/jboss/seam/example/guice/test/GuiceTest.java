c2f5780fcfc8b051cf15e605cd36920c713ab8db
Marek Schmidt <maschmid@redhat.com>
2012-04-20 08:03:10 +0000

    guice example fixes

--- examples-ee6/guice/guice-tests/src/test/java/org/jboss/seam/example/guice/test/GuiceTest.java
+++ examples-ee6/guice/guice-tests/src/test/java/org/jboss/seam/example/guice/test/GuiceTest.java
@@ -1,12 +1,24 @@
 package org.jboss.seam.example.guice.test;
 
+import java.io.File;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.OverProtocol;
+import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.seam.example.guice.JuiceBar;
 import org.jboss.seam.example.guice.Juice;
 import org.jboss.seam.example.guice.AppleJuice;
 import org.jboss.seam.example.guice.Orange;
 import org.jboss.seam.example.guice.OrangeJuice;
-import org.jboss.seam.mock.SeamTest;
-import org.testng.annotations.Test;
+import org.jboss.seam.mock.JUnitSeamTest;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.importer.ZipImporter;
+import org.jboss.shrinkwrap.api.spec.EnterpriseArchive;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import com.google.inject.Injector;
 import com.google.inject.Key;
@@ -14,8 +26,29 @@ import com.google.inject.Key;
 /**
  * @author Pawel Wrzeszcz (pwrzeszcz [at] jboss . org)
  */
-public class GuiceTest extends SeamTest
+@RunWith(Arquillian.class)
+public class GuiceTest extends JUnitSeamTest
 {
+   @Deployment(name="GuiceTest")
+   @OverProtocol("Servlet 3.0")
+   public static Archive<?> createDeployment()
+   {
+      EnterpriseArchive ear = ShrinkWrap.create(ZipImporter.class, "seam-guice.ear").importFrom(new File("../guice-ear/target/seam-guice.ear"))
+            .as(EnterpriseArchive.class);
+
+      // Install org.jboss.seam.mock.MockSeamListener
+      WebArchive web = ear.getAsType(WebArchive.class, "guice-web.war");
+      web.delete("/WEB-INF/web.xml");
+      web.addAsWebInfResource("web.xml");
+
+      web.addClasses(GuiceTest.class);
+      
+      JavaArchive ejb =  ear.getAsType(JavaArchive.class, "guice-ejb.jar");
+      ejb.addClasses(JuiceTestBar.class);
+      
+      return ear;
+   }
+
    @Test
    public void testGuiceInjector() throws Exception
    {
