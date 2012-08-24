8434277592f969f73969fa7a7a8d823e2c834ad6
Marek Schmidt <maschmid@redhat.com>
2012-08-24 11:34:37 +0000

    remove jboss-seam-ui from the SeamTest tests

--- seam-integration-tests/src/test/java/org/jboss/seam/test/integration/Deployments.java
+++ seam-integration-tests/src/test/java/org/jboss/seam/test/integration/Deployments.java
@@ -2,6 +2,7 @@ package org.jboss.seam.test.integration;
 
 import java.io.File;
 
+import org.jboss.shrinkwrap.api.ArchivePath;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.StringAsset;
 import org.jboss.shrinkwrap.api.importer.ZipImporter;
@@ -9,27 +10,7 @@ import org.jboss.shrinkwrap.api.spec.WebArchive;
 
 public class Deployments {
    public static WebArchive defaultSeamDeployment() {
-      return ShrinkWrap.create(ZipImporter.class, "test.war").importFrom(new File("target/seam-integration-tests.war")).as(WebArchive.class)
-            .addAsWebInfResource(new StringAsset(
-                  "<jboss-deployment-structure>" +
-                        "<deployment>" +
-                        "<dependencies>" +
-                        "<module name=\"org.javassist\"/>" +
-                        "<module name=\"org.dom4j\"/>" +
-                        "</dependencies>" +
-                        "</deployment>" +
-                  "</jboss-deployment-structure>"), "jboss-deployment-structure.xml")
-                  .addAsResource("seam.properties")
-                  .addAsResource("components.properties")
-                  .addAsResource("messages_en.properties")
-                  .addAsResource("META-INF/persistence.xml")
-
-                  .addAsResource("hibernate.cfg.xml")
-                  .addAsWebInfResource("WEB-INF/components.xml", "components.xml")
-                  .addAsWebInfResource("WEB-INF/pages.xml", "pages.xml")
-                  .addAsWebInfResource("WEB-INF/web.xml", "web.xml")
-                  .addAsWebInfResource("WEB-INF/ejb-jar.xml", "ejb-jar.xml")
-                  .addAsWebInfResource("WEB-INF/jboss-seam-integration-tests-hornetq-jms.xml", "jboss-seam-integration-tests-hornetq-jms.xml");
+      return defaultSeamDeployment("WEB-INF/components.xml");
    }
    
    // Deployment that use the proper SeamListener instead of the MockSeamListener
@@ -90,7 +71,7 @@ public class Deployments {
    }
 
    public static WebArchive defaultSeamDeployment(String customComponentsXml) {
-      return ShrinkWrap.create(ZipImporter.class, "test.war").importFrom(new File("target/seam-integration-tests.war")).as(WebArchive.class)
+      WebArchive war = ShrinkWrap.create(ZipImporter.class, "test.war").importFrom(new File("target/seam-integration-tests.war")).as(WebArchive.class)
             .addAsWebInfResource(new StringAsset(
                   "<jboss-deployment-structure>" +
                         "<deployment>" +
@@ -111,5 +92,15 @@ public class Deployments {
                   .addAsWebInfResource("WEB-INF/web.xml", "web.xml")
                   .addAsWebInfResource("WEB-INF/ejb-jar.xml", "ejb-jar.xml")
                   .addAsWebInfResource("WEB-INF/jboss-seam-integration-tests-hornetq-jms.xml", "jboss-seam-integration-tests-hornetq-jms.xml");
+      
+      // Remove jboss-seam-ui for a Mock SeamTest test as it would cause Mojarra to initialize
+      for (ArchivePath path : war.getContent().keySet()) {
+         if (path.get().contains("jboss-seam-ui")) {
+            war.delete(path);
+            break;
+         }
+      }
+      
+      return war;
    }
 }
