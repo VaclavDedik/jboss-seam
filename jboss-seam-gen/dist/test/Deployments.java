b4420d69eb4c32cd2814e16636244ca817d41db4
Marek Novotny <hotmana76@gmail.com>
2012-09-10 19:24:39 +0000

    JBSEAM-5023 more additions

--- jboss-seam-gen/dist/test/Deployments.java
+++ jboss-seam-gen/dist/test/Deployments.java
@@ -2,19 +2,23 @@ package @testPackage@;
 
 import java.io.File;
 
+import org.jboss.shrinkwrap.api.ArchivePath;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.StringAsset;
 import org.jboss.shrinkwrap.api.importer.ZipImporter;
-import org.jboss.shrinkwrap.api.spec.EnterpriseArchive;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 
+package org.jboss.seam.example.booking.test;
+
+import java.io.File;
+
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.EnterpriseArchive;
+import org.jboss.shrinkwrap.api.importer.ZipImporter;
+
 public class Deployments {
-   public static EnterpriseArchive defaultEarDeployment() {
-      return ShrinkWrap.create(ZipImporter.class, "@projectName@.ear").importFrom(new File("@workspaceHome@/@projectName@/test-build/@projectName@.ear"))
+   public static EnterpriseArchive bookingDeployment() {
+      return ShrinkWrap.create(ZipImporter.class, "${project.name}.${project.type}").importFrom(new File("${workspace.home/dist/${project.name}.${project.type}"))
             .as(EnterpriseArchive.class);
-   }
-   
-   public static WebArchive defaultWarDeployment() {
-      return ShrinkWrap.create(ZipImporter.class, "@projectName@.war").importFrom(new File("@workspaceHome@/@projectName@/test-build/@projectName@.war"))
-            .as(WebArchive.class);
    }
 }
