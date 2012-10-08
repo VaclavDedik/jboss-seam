db93a33cab9487c57559648a6b4492d6601b230f
Marek Schmidt <maschmid@redhat.com>
2012-10-08 11:50:42 +0000

    quartz example cleanup

--- examples/quartz/quartz-tests/src/test/java/org/jboss/seam/example/quartz/test/AccountTest.java
+++ examples/quartz/quartz-tests/src/test/java/org/jboss/seam/example/quartz/test/AccountTest.java
@@ -11,8 +11,6 @@ import org.jboss.seam.mock.DBJUnitSeamTest;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.spec.EnterpriseArchive;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
-import org.jboss.shrinkwrap.resolver.api.DependencyResolvers;
-import org.jboss.shrinkwrap.resolver.api.maven.MavenDependencyResolver;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
