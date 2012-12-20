1d5c6aa8d94a08ed40119df594ebfc256dfb7126
Vaclav Dedik <vdedik@redhat.com>
2012-12-20 16:29:46 +0100

    JBSEAM-5034

--- jboss-seam-gen/dist/test/ActionTest.java
+++ jboss-seam-gen/dist/test/ActionTest.java
@@ -4,23 +4,35 @@ import org.junit.Test;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.OverProtocol;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Filters;
+import org.jboss.shrinkwrap.api.GenericArchive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.importer.ExplodedImporter;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.jboss.seam.mock.JUnitSeamTest;
-import @testPackage@.Deployments;
 import @actionPackage@.@interfaceName@;
-//import @actionPackage@.@beanName@;
+import @actionPackage@.@beanName@;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.runner.RunWith;
+import java.io.File;
 
 @RunWith(Arquillian.class)
 public class @interfaceName@Test extends JUnitSeamTest {
+
+   public static final String WEBAPP_SRC = "@workspaceHome@/@projectName@/test-build";
+   public static final String PROJECT_HOME = "@workspaceHome@/@projectName@/";
 
    @Deployment(name="@interfaceName@Test")
    @OverProtocol("Servlet 3.0") 
    public static WebArchive createDeployment()
    {
-//      return Deployments.defaultDeployment()
-//            .addClasses(@interfaceName@.class, @beanName@.class)
-//            .addAsResource("seam.properties");
+        WebArchive war = ShrinkWrap.create(WebArchive.class, "test.war");
+        war.merge(ShrinkWrap.create(GenericArchive.class).as(ExplodedImporter.class)
+            .importDirectory(WEBAPP_SRC).as(GenericArchive.class), "/", Filters.includeAll())
+            .addClasses(@interfaceName@.class, @beanName@.class)
+            .addAsLibraries(@libraryList@)
+            .addAsResource("seam.properties");
+        return war;
    }
    
    
